bd5949c23c3436fa1c5509f8b3acbd236d4c4409
ProkopHapala <ProkopHapala@gmail.com>
2022-02-01 16:40:43 +0100

    FireCoreVisual in cpp/apps/Molcular can load libFireCore.so and run firecore_init() & firecore_evalForce() as well as Python

--- tests/t02_CH4/run.sh
+++ tests/t02_CH4/run.sh
@@ -4,18 +4,17 @@ rm answer.bas answer.xyz params.dat CHARGES *.out
 
 fireball="../../build/fireball.x"
 fireball_py="python ../../pyBall/FireCore.py"
+fireball_cpp="../../cpp/Build/apps/MolecularEditor/FireCoreVisual"
 
 
-#PATH=$PATH:/home/prokop/SW/intel/mkl/lib/intel64
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/prokop/SW/intel/mkl/lib/intel64
-#LD_LIBRARY_PATH=/home/prokop/SW/intel/mkl/lib/intel64:$LD_LIBRARY_PATH
-#LD_LIBRARY_PATH=/home/prokop/intel/mkl/lib/intel64:$LD_LIBRARY_PATH
+
+MKL_PATH=$PATH:/home/prokop/SW/intel/mkl/lib/intel64
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MKL_PATH
 export $LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 
-
 rm answer.bas answer.xyz params.dat CHARGES *.out
 
-
-$fireball    | tee relaxation.out
-$fireball_py | tee relaxation.py.out
+#$fireball    | tee relaxation.out
+#$fireball_py | tee relaxation.py.out
+$fireball_cpp | tee relaxation.cpp.out
