9097ae2bad95d055c12277dd512d94a9eea7e0d6
ProkopHapala <ProkopHapala@gmail.com>
2022-07-29 17:04:05 +0200

    FireCoreVisual : trying to make work textInput into lattice table

---
+++ tests/tQMMM_diacetylene/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fireball_cpp_2="../../cpp/Build/apps/MolecularEditor/FireCoreMMFFmini"
+FireCoreVisual="../../cpp/Build/apps/MolecularEditor/FireCoreVisual"
+
+MKL_PATH=$PATH:/home/prokop/SW/intel/mkl/lib/intel64
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MKL_PATH
+export $LD_LIBRARY_PATH
+#echo $LD_LIBRARY_PATH
+
+rm answer.bas answer.xyz params.dat CHARGES *.out
+
+#$fireball_cpp_2
+$FireCoreVisual
