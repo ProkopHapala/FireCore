3a34cfe153cc8a8d86aeb0188598fe6a5c59aee6
ProkopHapala <ProkopHapala@gmail.com>
2022-09-13 11:32:28 +0200

    pyBall/Fortran: adapted machine files to new ubuntu 2022LTS where are intel MKL libraries present

---
+++ pyBall/Machinefiles/simple_ubuntu_2022.sh
@@ -0,0 +1,3 @@
+INCLUDES = 
+LFLAGS   = -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -Bdynamic
+
--- pyBall/Makefile_machines.py
+++ pyBall/Makefile_machines.py
@@ -1,3 +1,17 @@
+'''
+# === OLD BAK
+PATH_MKL = /home/prokop/SW/intel/mkl
+PATH_MPI = /usr/lib/x86_64-linux-gnu/openmpi
+LPATHS  =  -L$(PATH_MKL)/lib/intel64 -I$(PATH_MKL)/include/fftw -L$(PATH_MPI)/lib -I$(PATH_MPI)include 
+LFLAGS_ =  -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lfftw3xf_gnu -lm -Bdynamic -lmpi 
+LFLAGS  = $(LPATHS) $(LFLAGS_) 
+#LFLAGS =  -L/home/prokop/SW/intel/mkl/lib/intel64 -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -L/home/prokop/intel/mkl/intel64 -I/home/prokop/intel/mkl/include/fftw -lfftw3xf_gnu -lm -Bdynamic -L/usr/lib/x86_64-linux-gnu/openmpi/lib -I/usr/lib/x86_64-linux-gnu/openmpi/include -lmpi 
+INCLUDES = -I/usr/include/mpich/
+
+# === NEW SIMPLYFIED
+LFLAGS   = -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -Bdynamic
+INCLUDES = 
+'''
 
 MKL_PATH = "/home/prokop/SW/intel"
 MPI_PATH = "/usr/lib/x86_64-linux-gnu/openmpi"
@@ -11,12 +25,10 @@ def genLFLAGS( MKL_PATH=MKL_PATH, MPI_PATH=MPI_PATH ):
         "PATH_MPI_LIB"   : MPI_PATH+"/lib",
         "PATH_MPI_INC"   : MPI_PATH+"/include",
     }
-    #print path_dict
-     
+    #print( path_dict )
+
     LFLAGS   = " -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lfftw3xf_gnu -lm -Bdynamic -lmpi "
-    #LPATHS  = "-L$(PATH_MKL) -L$(PATH_FFTW_LIB) -I$(PATH_FFTW_INCL) -L$(PATH_MPI_LIB) -I$(PATH_MPI_INC)"
     LPATHS   = " -L{PATH_MKL} -L{PATH_FFTW_LIB} -I{PATH_FFTW_INCL} -L{PATH_MPI_LIB} -I{PATH_MPI_INC} ".format(**path_dict)
-    #LFLAGS   = LPATHS + LFLAGS
     return LFLAGS, LPATHS
 
 MODLAP95   = "-I/usr/local/lib/lapack95_modules"
@@ -24,8 +36,10 @@ BLAS       = "-lblas"
 MODMPI     = "-I/usr/local/mpich/include"
 VISFLAGS   = ""
 
-FFLAG_ALL = " -fPIC "
 
+
+'''
+FFLAG_ALL = " -fPIC "
 _FFLAGS = {
     'F90'      : FFLAG_ALL+" -freal-4-real-8 -ffree-form -ffree-line-length-none ",
     'F77'      : FFLAG_ALL+" -freal-4-real-8 ",
@@ -35,6 +49,19 @@ _FFLAGS = {
     'DEBUGbak' : FFLAG_ALL+" -Og -g -fbounds-check -Wall -Wno-tabs",
     'DEBUG'    : FFLAG_ALL+" -Og -g -fcheck=all -fbounds-check -Wall -Wno-tabs -Wno-unused-variable -Wno-unused-label -Wno-missing-include-dirs",
     'DEBUGw'   : FFLAG_ALL+" -Og -g -fcheck=all -fbounds-check ",
+}
+'''
+
+FFLAG_ALL = " -fPIC -freal-4-real-8 "
+_FFLAGS = {
+    'F77'      : FFLAG_ALL+" ",
+    'F90'      : FFLAG_ALL+" -ffree-form -ffree-line-length-none ",
+    'CC'       : FFLAG_ALL+" -ffree-form -ffree-line-length-none ",
+    'OPT'      : " -O3 -mtune=native -ftree-vectorize -pg",
+    'VERY_OPT' : " -Ofast -march=native -mtune=native",
+    'DEBUGbak' : " -Og -g -fbounds-check -Wall -Wno-tabs",
+    'DEBUG'    : " -Og -g -fcheck=all -fbounds-check -Wall -Wno-tabs -Wno-unused-variable -Wno-unused-label -Wno-missing-include-dirs",
+    'DEBUGw'   : " -Og -g -fcheck=all -fbounds-check ",
 }
 
 _LFLAGS = {
