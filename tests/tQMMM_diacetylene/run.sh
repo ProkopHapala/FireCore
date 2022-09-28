c6f0f77e30191757920da53e2eef8e2fb98a1ab8
Prokop Hapala <ProkopHapala@gmail.com>
2022-09-28 12:04:28 +0200

    updated README

--- tests/tQMMM_diacetylene/run.sh
+++ tests/tQMMM_diacetylene/run.sh
@@ -2,8 +2,8 @@
 
 fireball_cpp_2="../../cpp/Build/apps/MolecularEditor/FireCoreMMFFmini"
 #FireCoreVisual="../../cpp/Build/apps/MolecularEditor/FireCoreVisual"
-FireCoreVisual="../../cpp/Build_OCL/apps/MolecularEditor/FireCoreVisual"
-FireCoreVisualOCL="../../cpp/Build_OCL/apps_OCL/MolecularEditorOCL/FireCoreVisualOCL"
+FireCoreVisual="../../cpp/Build/apps/MolecularEditor/FireCoreVisual"
+FireCoreVisualOCL="../../cpp/Build/apps_OCL/MolecularEditorOCL/FireCoreVisualOCL"
 
 MKL_PATH=$PATH:/home/prokop/SW/intel/mkl/lib/intel64
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MKL_PATH
