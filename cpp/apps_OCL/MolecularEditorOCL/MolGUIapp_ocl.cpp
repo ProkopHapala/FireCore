62f8a00a0ddb61549d29f4b6f0d99b47e1f86cd8
ProkopHapala <ProkopHapala@gmail.com>
2022-10-25 18:15:58 +0200

    started apps_OCL/MolGUIapp_ocl using MolWorld_sp3_ocl.h derived class from MolWorld_sp3.h with OpenCL OCL_PP.h for GPU acclerated GridFF; But initialization crashes

---
+++ cpp/apps_OCL/MolecularEditorOCL/MolGUIapp_ocl.cpp
@@ -0,0 +1,56 @@
+
+int verbosity = 0;
+
+#include "MolGUI.h"
+#include "MolWorld_sp3_ocl.h"
+
+#include "argparse.h"
+
+
+//MMFFsp3 W;
+MolGUI* app=0;
+LambdaDict funcs;
+
+int main(int argc, char *argv[]){
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_GL_SetAttribute(SDL_GL_SHARE_WITH_CURRENT_CONTEXT, 1);
+	//SDL_SetRelativeMouseMode( SDL_TRUE );
+	int junk;
+
+    SDL_DisplayMode DM;
+    SDL_GetCurrentDisplayMode(0, &DM);
+
+    //char* ssmile = 0; if(argc>1){ ssmile=argv[1]; } // initialize with smiles ?
+	//app = new MolGUI( junk, DM.w-100, DM.h-100, NULL, ssmile );
+	//if(argc>2){ app->W->loadSurf( argv[2] ); }      // load surface
+	
+	MolWorld_sp3_ocl* W = W = new MolWorld_sp3_ocl();
+
+	// --------- using argparse & LabdaDict;
+	app = new MolGUI( junk, DM.w-100, DM.h-100, W );
+	funcs["-s"]=[&](const char* s){ app->W->smile_name=s; }; // molecule
+	funcs["-x"]=[&](const char* s){ app->W->xyz_name=s;   }; // substrate
+	funcs["-g"]=[&](const char* s){ app->W->surf_name=s;  }; // substrate
+	//funcs["-l"]=[&](const char* s){ app->W->lvs_name=s;   }; // substrate
+	process_args( argc, argv, funcs );
+
+	app->init();
+	app->loop( 1000000 );
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
