f72c64abf87bc64094dc1a3d7e1695e7b8fb77ee
Prokop Hapala <ProkopHapala@gmail.com>
2023-02-22 09:35:38 +0100

    tests/tMolGUIapp

---
+++ tests/tMolGUIapp/run.sh
@@ -0,0 +1,28 @@
+name=MolGUIapp
+dir=../../cpp/Build/apps/MolecularEditor
+ln -s ../../cpp/common_resources data
+ln -s ../../cpp/common_resources common_resources 
+
+wd=`pwd`
+cd $dir
+pwd
+rm $name
+make $name
+cd $wd
+
+ln -s $dir/$name .
+
+#rm *.bin *.xsf
+
+#./$name -x common_resources/pyridine -n -g common_resources/NaCl_sym-center
+#./$name -x common_resources/pyridine -n 110 -g common_resources/NaCl_sym-center
+#./$name -x common_resources/polymer-2 -n 221 -g common_resources/NaCl_sym-center
+#./$name -x common_resources/polydiacetylene -n 221 -g common_resources/NaCl_sym-center
+#./$name -x common_resources/polydiacetylene -n 241 -g common_resources/NaCl_564
+#./$name -x common_resources/polydiacetylene -n 241 -g common_resources/NaCl_564 -ng 4,-2,2,3
+#./$name -x common_resources/polydiacetylene -n 141 -g common_resources/NaCl_564 -ng 2,-1,2,3
+#./$name -x common_resources/polydiacetylene -n 141 -g common_resources/NaCl_1x1_L2 -ng 2,-1,3,4
+#./$name -x common_resources/polydiacetylene_OH -n 141 -g common_resources/NaCl_1x1_L2 -ng 2,-1,3,4
+#./$name -x common_resources/polydiacetylene_OH -g common_resources/NaCl_1x1_L2
+./$name -x common_resources/polydiacetylene -g common_resources/NaCl_1x1_L2 -subs 4,common_resources/-COOH.xyz
+#./$name -x common_resources/polydiacetylene -g common_resources/NaCl_1x1_L2 -subs 4,common_resources/-COOH.xyz -n -141 -ng 2,-1,3,4
