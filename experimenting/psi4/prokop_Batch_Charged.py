88659b8c1bdb6b71eae8c33814339a75812fe48e
Prokop Hapala <ProkopHapala@gmail.com>
2023-03-09 15:08:35 +0100

    polished prokop_Batch_Charged.py

--- experimenting/psi4/prokop_Batch_Charged.py
+++ experimenting/psi4/prokop_Batch_Charged.py
@@ -1,9 +1,42 @@
 import psi4
 import resp
+import os
+import time
 
 # ========= Setup
 
-fname='H2O.xyz'
+indir="./input/"
+#outdir="./output/"
+
+#bRelax=True
+bRelax=False
+
+#method='scf'     # Hartree-Fock
+#method='pbe' 
+#method='b3lyp'
+#method='mp2'
+#basis='sto-3g'
+#basis='6-31+G'
+#basis='cc-pvdz'
+
+methods=[
+#    'scf',
+#    'pbe',
+#    'b3lyp',
+    'mp2'
+]
+basises=[
+    # 'sto-3g',
+    # '6-31+G',
+    # '6-311+G*',
+    # '6-311++G**',
+    # '6-311++G(3df,3pd)',
+     'cc-pvdz',
+    # 'aug-cc-pvtz',
+    # 'def2-QZVPPD',
+]
+
+
 resp_options = {
 'VDW_SCALE_FACTORS'  : [1.4, 1.6, 1.8, 2.0],
 'VDW_POINT_DENSITY'  : 1.0,
@@ -21,6 +54,8 @@ psi4_options = {
 }
 
 
+
+
 names=[
 # ---- Minimal
 #"HF",
@@ -31,9 +66,9 @@ names=[
 #"oxalate",
 #"F-COOH",
 #"NC-COOH",
-#"HCOOH",
+"HCOOH",
 #"OCH2",
-"formaldimine",
+#"formaldimine",
 # ------- Other
 #"acetic_acid",
 #"acetimide",
@@ -60,82 +95,71 @@ names=[
 
 # ======== Functions
 
-def file2string(fname):
-    ls = [ l for l in open(fname) ]
-    return '\n'.join(ls[2:])
+#def file2string(fname):
+#    ls = [ l for l in open(fname) ]
+#    return '\n'.join(ls[2:])
+
+def try_make_dirs( dname ):
+    try:
+        os.mkdir( dname )
+    except:
+        pass
+
+def xyz2str(fname):
+    ls = [ " ".join( l.split()[:4] ) for l in open(fname).readlines()[2:] ]
+    #print(ls)
+    return '\n'.join(ls)
 
 def save_xyz_Q( fname, lines, Qs ):
     n = len(Qs)
-    print("lines ", lines)
-    print("Qs ",Qs)
+    #print("lines ", lines)
+    #print("Qs ",Qs)
     with open(fname,'w') as fout:
         fout.write("%i\n" %n)
         fout.write("#comment \n" )
         for i,Q in enumerate( Qs ):
             fout.write( "%s %10.5f \n" %(lines[i],  Q) )
 
-def process_molecule( name, bRelax=True ):
-    print( "# ======= Molecule: ", name )
+def process_molecule( name, bRelax=True, indir="./input/", outdir="./output/", method='scf', basis='/STO-3G' ):
+    method_basis = method+"/"+basis;    #print( method_basis )
     # ------ load geometry
-    #geom = file2string("./molecules/"+name)
-    geom  = file2string("./molecules/"+name+".xyz")
+    geom  = xyz2str( indir+name+".xyz")     #;print("geom>>%s<<" %geom )
     mol   = psi4.geometry( geom )
-    #mol  = psi4.Molecule.init_with_xyz(filen) 
     mol.update_geometry()
-    
-    psi4.set_options( psi4_options )
+    mol.symmetrize(1e-3)   # this heps prevent problems with symmetry : https://github.com/psi4/psi4webmo/issues/4
 
+    psi4.set_options( psi4_options )
+    
+    # ------ relax geometry
     if bRelax:
-        #psi4.optimize('scf/cc-pvdz', molecule=mol)
-        psi4.optimize('scf/STO-3G', molecule=mol)
-        #psi4.optimize('scf/6-311pg_2df_2pd_', molecule=mol, symmetry='c1' )
-        #psi4.optimize('scf/cc-pvdz', molecule=mol, engine='geometric' )
-        #psi4.optimize('pbe/cc-pvdz', molecule=mol)
-        #psi4.optimize('b3lyp/cc-pvdz', molecule=mol)
-        #psi4.optimize('mp2/cc-pvdz', molecule=mol)
-        #mol.save_xyz_file('relaxed/'+fname,1)
+        psi4.optimize(method_basis, molecule=mol)
 
     # ----- save output
     geom_lines = mol.save_string_xyz().split('\n')[1:]
 
     # ------------ Call for first stage fit
-    Qs = resp.resp([mol], resp_options)
-    Q_esp  = Qs[0] 
-    Q_resp = Qs[1]
-    print('Electrostatic Potential Charges\n',            Q_esp)
-    print('Restrained Electrostatic Potential Charges\n', Q_resp)
-
-    save_xyz_Q( "relaxed/"+name+".xyz", geom_lines, Q_resp )
+    resp_options['METHOD_ESP'] = method
+    resp_options['BASIS_ESP' ] = basis
+    Qs = resp.resp([mol], resp_options)   ;Q_esp=Qs[0]; Q_resp=Qs[1]
+    print('ESP  Charges: ', Q_esp  )
+    print('RESP Charges: ', Q_resp )
+    save_xyz_Q( outdir+name+".xyz", geom_lines, Q_resp )
 
 # ======== Main
 
 #psi4.core.be_quiet()
 
-
-
-for name in names:
-    psi4.core.set_output_file(name+'.log', False)
-    process_molecule( name, bRelax=True )
-
-
-'''
-# Change the value of the RESP parameter A
-options['RESP_A'] = 0.001
-
-# ----------- Add constraint for atoms fixed in second stage fit
-constraint_charge = []
-for i in range(4, 8):
-    constraint_charge.append([charges1[1][i], [i+1]])
-options['constraint_charge'] = constraint_charge
-options['constraint_group'] = [[2, 3, 4]]
-options['grid'] = ['1_%s_grid.dat' %mol.name()]
-options['esp'] = ['1_%s_grid_esp.dat' %mol.name()]
-
-# Call for second stage fit
-charges2 = resp.resp([mol], options)
-
-# Get RESP charges
-print("\nStage Two:\n")
-print('RESP Charges')
-print(charges2[1])
-'''
+for method in methods:
+    for basis in basises:
+        outdir = method+"/"+basis+"/"
+        try_make_dirs( method )
+        try_make_dirs( outdir )
+        for name in names:
+            print( "# ======= Molecule: ", name, method, basis )
+            psi4.core.set_output_file(outdir+name+'.log', False)
+            t0 = time.time_ns()
+            try:
+                process_molecule( name, bRelax=bRelax, method=method, basis=basis, indir=indir, outdir=outdir )
+            except Exception as e: 
+                print(e)
+            t = time.time_ns() - t0; print( "time: ", t*1e-9, "[s]" )
