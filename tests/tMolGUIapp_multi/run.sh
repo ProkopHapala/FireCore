54b865214b42b5075618a3248ea5899c5451e61e
Prokop Hapala <ProkopHapala@gmail.com>
2023-03-13 18:46:39 +0100

    cleaned tMolGUIapp_multi

---
+++ tests/tMolGUIapp_multi/run.sh
@@ -0,0 +1,18 @@
+name=MolGUIapp_multi
+dir=../../cpp/Build/apps_OCL/MolecularEditorOCL
+ln -s ../../cpp/common_resources data
+ln -s ../../cpp/common_resources common_resources 
+
+wd=`pwd`
+cd $dir
+pwd
+rm $name
+make -j4 $name
+cd $wd
+
+ln -s $dir/$name .
+
+#rm *.bin *.xsf
+
+#./$name -x common_resources/formic_dimer
+./$name -x common_resources/HCOOH
