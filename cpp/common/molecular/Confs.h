c3835a213a5c873c86a0a531b3a5fd4c25927589
Prokop Hapala <ProkopHapala@gmail.com>
2023-03-18 19:54:25 +0100

    started Confs.h for computing distance and force between different replicas of the system; usefull configuration search algorithms such as 1) Dimer Method 2) Metadynamics 3) Particle-Swarm ... etc

---
+++ cpp/common/molecular/Confs.h
@@ -0,0 +1,132 @@
+
+#ifndef Confs_h
+#define Confs_h
+
+#include "quaternion.h"
+
+class Group{ public:
+    int     n = 0;   // number of atoms
+    int*    iatoms;  // atom indexes belonging to the group
+    Quat4f* cs;      // projection coefficients
+
+    inline float distance2( Quat4f* ps1, Quat4f* ps2 )const{
+        float lr2 = 0.f; 
+        for(int i=0; i<n; i++){
+            Vec3f d = ps1[i].f - ps2[i].f;
+            //lr2 += d.norm2()*w;  // weighted?
+            lr2 += d.norm2();
+        }
+        return lr2;
+    }
+
+    inline Vec3f cog( Quat4f* ps )const{
+        Vec3f cog = Vec3fZero; 
+        for(int i=0; i<n; i++){
+            int ia = iatoms[i];
+            //cog.add_mul( ps[ia].f, w );       // weighted?
+            cog.add( ps[ia].f ); 
+        }
+        return cog;
+    }
+
+    void pose( Quat4f* ps, Vec3f& cog, Vec3f& u, Vec3f& v )const{
+        cog = Vec3fZero;
+        u   = Vec3fZero;
+        v   = Vec3fZero;
+        Quat4f Csum = Quat4fZero;
+        for(int i=0; i<n; i++){
+            int ia          = iatoms[i];
+            const Quat4f& C = cs[i];
+            const Vec3f&  p = ps[ia].f;
+            Csum.add(C);
+            cog.add_mul( p, C.w );
+            u.add_mul  ( p, C.x );
+            v.add_mul  ( p, C.y );
+        }
+        cog.mul( 1/Csum.w );
+        u.add_mul( cog, -Csum.x );
+        v.add_mul( cog, -Csum.y );
+    }
+
+    void apply_pose_force( Quat4f* fs, const Vec3f& fcog, const Vec3f& fu, const Vec3f& fv )const{
+        for(int i=0; i<n; i++){
+            int ia          = iatoms[i];
+            const Quat4f& C = cs    [i];
+            Vec3f fi = Vec3fZero;
+            fi.add_mul( fcog, C.w );
+            fi.add_mul( fu,   C.x ); // what about subtracted cog ?
+            fi.add_mul( fv,   C.y );
+            fs[i].f.add(fi);
+        }
+    }
+
+}; // class Group{
+
+class Confs{ public:
+    int nSys  = 0;  // number of systems
+    int natom = 0;  // number of atoms
+    int nvec  = 0;  // number of quaternion per system
+
+    Vec3f KL{ 1.0, 1.0, 1.0 };  // relative weight of translation and rotation distance
+
+    Quat4f* atoms      =0;
+    Quat4f* aforces    =0;
+
+    std::vector<Group> groups;
+
+    float dist2( int isys1, int isys2 ){   // distance between two systems
+        int ng = groups.size();
+        int i1 = isys1*nvec;
+        int i2 = isys2*nvec;
+        float l2 = 0; 
+        for(int ig=0; ig<ng; ig++ ){
+            const Group& G = groups[ig];
+            //Vec3f L = pose_distance( ps1, ps2 );
+            Vec3f cog1, u1, v1; G.pose( atoms+i1, cog1, u1, v1 );
+            Vec3f cog2, u2, v2; G.pose( atoms+i2, cog2, u2, v2 );
+            Vec3f L2 = Vec3f{ cog1.dist2(cog2), 1-u1.cos(u2), 1-v1.cos(v2) };
+            l2     += L2.dot( KL );
+        }
+        return l2;
+    }
+
+    float collision_force( int isys1, int isys2, float l0, float k ){   // distance between two systems
+        int ng = groups.size();
+        Vec3f Ls[ng];
+        int i1 = isys1*nvec;
+        int i2 = isys2*nvec;
+        float l2 = 0; 
+        // ---- evaluate configuration distances (reduction)
+        for(int ig=0; ig<ng; ig++ ){
+            const Group& G = groups[ig];
+            //Vec3f L = pose_distance( ps1, ps2 );
+            Vec3f cog1, u1, v1; G.pose( atoms+i1, cog1, u1, v1 );
+            Vec3f cog2, u2, v2; G.pose( atoms+i2, cog2, u2, v2 );
+            Vec3f L2 = Vec3f{ cog1.dist2(cog2), 1-u1.cos(u2), 1-v1.cos(v2) };
+            Ls[ig].set_mul(L2,KL); 
+            l2      += L2.dot( KL );
+
+        }
+        // ---- distance to force
+        float l   = sqrt(l2);
+        float dl  = l - l0;
+        float f_r = k*dl/l;
+        // ---- apply force
+        for(int ig=0; ig<ng; ig++ ){
+            const Group& G = groups[ig];
+            Vec3f fcog1;  // TO BE DONE 
+            Vec3f fu1;    // TO BE DONE
+            Vec3f fv1;    // TO BE DONE
+            G.apply_pose_force( aforces+i1, fcog1, fu1, fv1 );
+            Vec3f fcog2;  // TO BE DONE
+            Vec3f fu2;    // TO BE DONE
+            Vec3f fv2;    // TO BE DONE
+            G.apply_pose_force( aforces+i2, fcog2, fu2, fv2 );
+        }
+        return l;
+    }
+
+};   // class Confs{ public: 
+
+
+#endif
