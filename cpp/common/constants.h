07771ac4f114e27b6fafe32fa204d5ba232861cb
Prokop Hapala <ProkopHapala@gmail.com>
2023-05-09 22:31:49 +0200

    forgot to add

---
+++ cpp/common/constants.h
@@ -0,0 +1,11 @@
+
+#ifndef  constants_h
+#define  constants_h
+
+#define GOLDEN_RATIO    1.618033988749894
+#define deg2rad         0.01745329251994329547
+#define rad2deg        57.29577951308232286465
+
+#define COULOMB_CONST  14.3996448915
+
+#endif
