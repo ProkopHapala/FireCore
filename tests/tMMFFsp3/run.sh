b147275f7eed31e633d5939168ce6edc19ab8803
Paolo Nicolini <niko_pd@yahoo.it>
2023-05-23 11:25:54 +0200

    commented out more unnecessary changes

--- tests/tMMFFsp3/run.sh
+++ tests/tMMFFsp3/run.sh
@@ -17,11 +17,11 @@ cd $wd
 python3 run_Eprofiles.py
 
 #dir=/home/niko/work/CARBSIS/2-molecules/data_Paolo/uff_files/test_UFF/CHONH2
-
+#
 #mol='CHONH2'
-
+#
 #ds='-0.30 -0.25 -0.20 -0.15 -0.10 -0.05 0.00 0.05 0.10 0.15 0.20 0.25 0.30'
-
+#
 #rm -f x.xyz
 #mkdir -p $mol
 #for d in $ds
@@ -30,7 +30,7 @@ python3 run_Eprofiles.py
 #    ln -sf $dir/improper$d/run.xyz x.xyz
 #    python3 eval_E_xyz.py x | grep MYOUTPUT 1> $mol/out.$d
 #done
-
+#
 #rm x.xyz
-
-exit
+#
+#exit
