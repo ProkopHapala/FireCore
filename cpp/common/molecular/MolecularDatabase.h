b372406d7871ea449dac2d17875c1a764d1518b4
Milan Koci <kocimil1@fjfi.cvut.cz>
2024-01-04 17:38:34 +0100

    Beggining of the database

--- cpp/common/molecular/MolecularDatabase.h
+++ cpp/common/molecular/MolecularDatabase.h
@@ -8,76 +8,87 @@
 #include "Atoms.h"
 #include "Vec3Utils.h"
 
-class MolecularDatabase : public Atoms
-{
+
+class MolecularDatabase : public MetaData{
 private:
-    int* key;
-    DatabaseMember* members;
-    int nMembers;
-
-public:
-    MolecularDatabase();
-    ~MolecularDatabase();
-    void search();
-    void duplicateCheck();
-    void addMember(double E, Vec3d& F, Atoms& atoms);
-    void addMember(DatabaseMember& member);
-    int getNMembers(){ return this->nMembers; };
-    DatabaseMember* getMember(int i){ return &this->members[i]; };
-
-};
-
-
-
-
-class DatabaseMember : public MolecularDatabase{
-    public:
-    int    key;
-    double E;
-    Vec3d  F;
+    Vec3d* descriptors;
     Atoms* atoms;
-    Vec3d principalAxes;
-    DatabaseMember(double E, Vec3d& F, Atoms& atoms){
-        this->E = E;
-        this->F = F;
-        this->atoms = &atoms;
-        calculatePrincipalAxes();
+    int nMembers;
+    int nMaxCell;
+public:
+    MolecularDatabase(){
+        nMaxCell = 100;
+        alloc(nMaxCell);
     };
-    //DatabaseMember(int natoms){ realloc(natoms); };
-    //DatabaseMember(const DatabaseMember& p){ copyOf(p);  };
-    //~DatabaseMember(){};
-
-    /**
-     * Calculates the principal axes of the molecular database.
-     * This function calculates the principal axes of the molecular database
-     * using the moment of inertia tensor and stores the result in the
-     * `principalAxes` variable.
-     * @f[I=(r_i)^2*E-r_i⊗r_i@f] 
-     *  
-     * 
-     */
-    void calculatePrincipalAxes(){
-        int natoms = atoms->natoms;
+    MolecularDatabase(int n){
+        alloc(n);
+    };
+    void alloc(int n){
+        this->nMembers = 0;
+        this->nMaxCell = n;
+        this->atoms = new Atoms[n];
+        this->descriptors = new Vec3d[n];
+    };
+    void realloc(int n){
+        this->nMaxCell = n;
+        Atoms* temp = new Atoms[n];
+        memcpy(temp, this->atoms, sizeof(Atoms)*n);
+        delete[] this->atoms;
+        this->atoms = temp;
+        Vec3d* temp2 = new Vec3d[n];
+        memcpy(temp2, this->descriptors, sizeof(Vec3d)*n);
+        delete[] this->descriptors;
+        this->descriptors = temp2;        
+    };
+    void addMember(Atoms structure){
+        if(nMembers >= nMaxCell){
+            realloc(nMaxCell*2);
+        }
+        atoms[nMembers].copyOf(structure);
+        calculatePrincipalAxes(&structure);
+        this->nMembers++;
+    };
+    void  print(){
+        printf("nMembers: %d\n", nMembers);
+        printf("%-10s %-10s %-10s %-10s %-10s\n", "Member", "Ax_1", "Ax_2", "Ax_3", "nAtoms");
+        for(int i = 0; i < nMembers; i++){
+            printf("%-10d %-10f %-10f %-10f %-10d\n", i, descriptors[i].x, descriptors[i].y, descriptors[i].z, atoms[i].natoms);
+        }
+    };
+    //virtual char* tostr(int id){};    
+    void calculatePrincipalAxes(Atoms* a){
         Mat3d I;
         I.set(0.0);
-        Vec3d ws = Vec3dOne;
-        Vec3d cog = Vec3dZero;
-        getCOG(natoms, atoms->apos, ws, cog);
+        int natoms = a->natoms;
+        double* ws = new double[natoms];
         for(int i=0; i<natoms; i++){
-            I.xx += (apos[i].y-cog.y)*(apos[i].y-cog.y) + (apos[i].z-cog.z)*(apos[i].z-cog.z);
-            I.xy += -(apos[i].x-cog.x)*(apos[i].y-cog.y);
-            I.xz += -(apos[i].x-cog.x)*(apos[i].z-cog.z);
-            I.yy += (apos[i].x-cog.x)*(apos[i].x-cog.x) + (apos[i].z-cog.z)*(apos[i].z-cog.z);
-            I.yz += -(apos[i].y-cog.y)*(apos[i].z-cog.z);
-            I.zz += (apos[i].x-cog.x)*(apos[i].x-cog.x) + (apos[i].y-cog.y)*(apos[i].y-cog.y);
+            //ws[i] = atypes[i];
+            ws[i] = 1.0;
         }
-        I.eigenValues(principalAxes);
+        Vec3d cog = Vec3dZero;
+        getCOG(natoms, a->apos, ws, cog);
+        for(int i=0; i<natoms; i++){
+            Vec3d r;
+            Mat3d E=Mat3dIdentity;
+            r.set_sub(a->apos[i], cog);
+            I.addOuter(r, r, -ws[i]);
+            I.add_mul(E,r.norm2()*ws[i]);
+        }
+        I.eigenvals(descriptors[nMembers]);
+        descriptors[nMembers].sort();
+        delete [] ws;
+        printf("%lf %lf %lf\n", descriptors[nMembers].x, descriptors[nMembers].y, descriptors[nMembers].z);
     }
-
-    Vec3d getPrincipalAxes(){ return this->principalAxes;};
 
 
 };
+
+
+
+
+
+
+
 
 
 
