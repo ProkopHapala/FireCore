3b28a2f72e15e6b5156cde58b3b205bf441f2481
Prokop Hapala <ProkopHapala@gmail.com>
2024-01-15 17:37:02 +0100

    returned back functionality to MolGUIapp

--- cpp/common/molecular/MolWorld_sp3.h
+++ cpp/common/molecular/MolWorld_sp3.h
@@ -1183,10 +1183,10 @@ virtual void MDloop( int nIter, double Ftol = 1e-6 ){
     //ffl.run( nIter, 0.05, 1e-6, 1000.0 );
     //ffl.run_omp( nIter, 0.05, 1e-6, 1000.0 );
 
-    run_no_omp( nIter, 0.05, 1e-6, 1000.0 );
+    //run_no_omp( nIter, 0.05, 1e-6, 1000.0 );
 
     //run_omp( 1, 0.05, 1e-6, 1000.0 );
-    //run_omp( nIter, 0.05, 1e-6, 1000.0 );
+    run_omp( nIter, 0.05, 1e-6, 1000.0 );
     
     //run_omp( 100, 0.05, 1e-6, 1000.0 );
     //run_omp( 1, opt.dt, 1e-6, 1000.0 );
