89161313682be1d2f320842490961a999400e3dc
Prokop Hapala <ProkopHapala@gmail.com>
2024-02-12 23:51:44 +0100

    RFFapp : created DiamondSurface111

--- cpp/common/dataStructures/Buckets3D.h
+++ cpp/common/dataStructures/Buckets3D.h
@@ -27,22 +27,31 @@ class Buckets3D : public Buckets, public CubeGridRuler { public:
     void pointsToCells( int np, Vec3d* ps, bool* ignore=0 ){
         //printf( "Buckets3D::pointsToCells(np=%i) @ps=%li @ignore=%li @obj2cell=%li \n", np, (long)ps, (long)ignore, (long)obj2cell );
         if( bResizing || (obj2cell==0) ){ Buckets::resizeObjs( np, true ); }
+        //printf( "Buckets3D::pointsToCells(np=%i) @ps=%li @ignore=%li @obj2cell=%li \n", np, (long)ps, (long)ignore, (long)obj2cell );
         if( np>nobjSize ){ printf( "Buckets3D::pointsToCells(bResizing=%i) ERROR np(%i)>nobjSize(%i) => Exit()\n", bResizing, np, nobjSize ); exit(0); }
         //printf( "Buckets3D::pointsToCells() obj2cell %li \n", (long)obj2cell  );
         if  (ignore){ 
             for( int i=0; i<np; i++ ){ 
-                //printf( "Buckets3D::pointsToCells()[i=%i]\n", i  );
-                bool bi = ignore[i];
-                if(!bi){ 
-                    int ic = icell( ps[i] );
-                    //if((ic<0)||(ic>=ncell  )){ printf( "Buckets3D::pointsToCells() ic=%i ncell=%i \n", ic, ncell ); }      // Debug
-                    //if((i <0)||(i>=nobjSize)){ printf( "Buckets3D::pointsToCells() i=%i nobjSize=%i \n", i, nobjSize ); } // Debug
-                    obj2cell[i] = ic; 
-                }
+                //printf( "Buckets3D::pointsToCells()[i=%i]  A\n", i  );
+                //bool bi = ignore[i];
+                //const int ic = icell( ps[i] );
+                int ic; 
+                if( ignore[i] ){ ic = -1; }else{ ic=icell_bound( ps[i] ); }
+                //if((ic<0)||(ic>=ncell  )){ printf( "Buckets3D::pointsToCells() ic=%i ncell=%i \n", ic, ncell ); }      // Debug
+                //if((i <0)||(i>=nobjSize)){ printf( "Buckets3D::pointsToCells() i=%i nobjSize=%i \n", i, nobjSize ); } // Debug
+                obj2cell[i] = ic; 
                 //if(!ignore[i])obj2cell[i] = icell( ps[i] );}
             } 
         }
-        else        { for( int i=0; i<np; i++ ){               obj2cell[i] = icell( ps[i] );} }
+        else { for( int i=0; i<np; i++ ){
+                //printf( "Buckets3D::pointsToCells()[i=%i]  B\n", i  );
+                //if(i>=nobjSize){ printf( "Buckets3D::pointsToCells() i=%i nobjSize=%i \n", i, nobjSize ); }   // Debug
+                //const int ic = icell( ps[i] );
+                const int ic = icell_bound( ps[i] );
+                //if((ic<0)||(ic>=ncell  )){ printf( "Buckets3D::pointsToCells() ic=%i ncell=%i \n", ic, ncell ); }      // Debug
+                obj2cell[i] = ic;
+            } 
+        }
         //printf( "Buckets3D::pointsToCells() np %i \n", np  );
         updateCells( np, obj2cell );
         //printf( "Buckets3D::pointsToCells() DONE\n"  );
--- cpp/common/math/quaternion.h
+++ cpp/common/math/quaternion.h
@@ -188,6 +188,7 @@ class Quat4T {
 
 // ====== Quaternion multiplication
 
+
 	inline void setQmul( const QUAT& a, const QUAT& b) {
         x =  a.x * b.w + a.y * b.z - a.z * b.y + a.w * b.x;
         y = -a.x * b.z + a.y * b.w + a.z * b.x + a.w * b.y;
@@ -228,6 +229,37 @@ class Quat4T {
         a.z = -x * ay + y * ax - z * aw + w * az;
         a.w = +x * ax + y * ay + z * az + w * aw;
     };
+
+    inline QUAT operator%( const QUAT& b)const{ 
+        return QUAT{
+             x * b.w + y * b.z - z * b.y + w * b.x,
+            -x * b.z + y * b.w + z * b.x + w * b.y,
+             x * b.y - y * b.x + z * b.w + w * b.z,
+            -x * b.x - y * b.y - z * b.z + w * b.w
+        };
+    }
+
+    inline VEC operator%( const VEC& v)const{  // Hamilton product ( q * v ) vector is rotated by quaternion
+        T tx = 2*( z*v.y - y*v.z  + w*v.x);
+        T ty = 2*( x*v.z - z*v.x  + w*v.y);
+        T tz = 2*( y*v.x - x*v.y  + w*v.z);
+        return VEC{
+            v.x + ty*z - tz*y,
+            v.y + tz*x - tx*z,
+            v.z + tx*y - ty*x
+        };
+    }
+
+    inline VEC operator|( const VEC& v )const{  // inverse Hamilton product ( v * q ) vector is rotated by quaternion backwards
+        T tx = 2*( y*v.z - z*v.y  + w*v.x);
+        T ty = 2*( z*v.x - x*v.z  + w*v.y);
+        T tz = 2*( x*v.y - y*v.x  + w*v.z);
+        return VEC{
+            v.x + y*tz - z*ty,
+            v.y + z*tx - x*tz,
+            v.z + x*ty - y*tx
+        };
+    }
 
     inline void transformVec( const VEC& vec, VEC& out ) const{
         //QUAT qv; qv.set(vec.x,vec.y,vec.z,0.0);
