ace9e58fbd350f015fc26b3571b99c2d28130893
Milan Koci <kocimil1@fjfi.cvut.cz>
2024-02-14 16:25:02 +0100

    Added rotational translational invariant comparison RMSE of molecules

--- cpp/common/molecular/Atoms.h
+++ cpp/common/molecular/Atoms.h
@@ -13,34 +13,13 @@ class Points{ public:
     Vec3d * ps=0;
 };
 
-enum DescriptorType {principal_axes, number_of_atoms};
 
-struct MolecularDatabaseDescriptor
-{
-    DescriptorType type;
-    int atom_type = -1;
-};
 
 class MetaData{ public:
    /*virtual void print()=0;
    virtual char* tostr(int id)=0;*/
-
-    MolecularDatabaseDescriptor* usedDescriptors=nullptr;
-    int nbOfusedDescriptors = 0;
     int dimensionOfDescriptorSpace = 0;
-// User can choose desriptors to use by writing it in the usedDescriptor array (chosen order is important):
-// first parameter is the type of descriptor (principal_axes, number_of_atoms)
-// second parameter is the atom type (-1 means all atoms)
-    MetaData(){
-        nbOfusedDescriptors = 3;
-        usedDescriptors = new MolecularDatabaseDescriptor[nbOfusedDescriptors];
-        usedDescriptors[0] = {principal_axes, -1};
-        usedDescriptors[1] = {number_of_atoms, -1};
-        usedDescriptors[2] = {number_of_atoms, -1};
-    };
-    ~MetaData(){
-        delete[] usedDescriptors;
-    };
+
 };
 
 
