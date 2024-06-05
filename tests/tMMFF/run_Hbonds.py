644d1e550c0086a89adfc8648ec8a14d17c00445
Prokop Hapala <ProkopHapala@gmail.com>
2024-06-05 17:21:21 +0200

    run_Hbonds.py plot against reference;  mmff.sampleHbond() returns string with info about atoms involved in the bond

--- tests/tMMFF/run_Hbonds.py
+++ tests/tMMFF/run_Hbonds.py
@@ -16,6 +16,11 @@ nmaxiter = 10000
 xyz_name = "polymer-2_new-OH"
 
 
+ref_path = "/home/prokop/Desktop/CARBSIS/PEOPLE/Mithun/HBond_Fit_Radial_Scans/energy_angle_0/"
+
+ref_O_NH = np.genfromtxt(ref_path+'ref-CH2O_NH3-000.dat')
+ref_N_NH = np.genfromtxt(ref_path+'ref-NH3_NH3-000.dat')
+
 #ialg  = 2
 #ialg  = 3
 #alg_names=["GD","MDdamp","FIRE","FIREsmooth"]
@@ -30,17 +35,24 @@ mmff.init( xyz_name="data/"+xyz_name, bMMFF=True )
 
 clrs = [ 'r','g','b','m','c' ]
 
-rs = np.linspace(1.5,5.5, 400 )
+rs = np.linspace(1.5,20.0, 100 )
 nb = mmff.findHbonds( Rcut=4.0, angMax=30.0 );
 for ib in range( nb ):
     c = clrs[ib]
-    Es,Fs = mmff.sampleHbond( ib, rs );               plt.plot( rs, Es - Es[-1], c=c, ls='-',  label="Hb #"+str(ib)         )
-    Es,Fs = mmff.sampleHbond( ib, rs, maskH=0.0 );    plt.plot( rs, Es - Es[-1], c=c, ls='--', label="Hb #"+str(ib)+" noHb" )
-    Es,Fs = mmff.sampleHbond( ib, rs, maskQ=0.0 );    plt.plot( rs, Es - Es[-1], c=c, ls=':',  label="Hb #"+str(ib)+" noQ"  )
+    Es,Fs,s = mmff.sampleHbond( ib, rs                       );    plt.plot( rs, Es - Es[-1], c=c, ls='-',  label="Hb #"+str(ib)         )
+    #Es,Fs,s = mmff.sampleHbond( ib, rs,           maskH=0.0  );    plt.plot( rs, Es - Es[-1], c=c, ls='--', label="Hb #"+str(ib)+" noHb" )
+    #Es,Fs,s = mmff.sampleHbond( ib, rs, maskQ=0.0            );    plt.plot( rs, Es - Es[-1], c=c, ls='-',  label="Hb #"+str(ib)+" noQ |"+s  )
+    #Es,Fs,s = mmff.sampleHbond( ib, rs, maskQ=0.0, maskH=0.0 );    plt.plot( rs, Es - Es[-1], c=c, ls=':',  label="Hb #"+str(ib)+" noQ |"+s  )
+    #print ,s
+
+plt.plot(ref_O_NH[:,0], ref_O_NH[:,1], label='ref_O_NH' )
+plt.plot(ref_N_NH[:,0], ref_N_NH[:,1], label='ref_N_NH' )
 
 plt.legend()
 plt.grid()
-plt.ylim(-0.1,0.1)
+plt.ylim(-0.3,0.3)
+#plt.ylim(-0.1,0.1)
+#plt.ylim(-0.03,0.03)
 
 plt.show()
 
