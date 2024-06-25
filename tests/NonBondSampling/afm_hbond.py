457ba924cc33b31ddb3759106216c758c236c670
Prokop Hapala <ProkopHapala@gmail.com>
2024-06-25 15:13:36 +0200

    added contour into afm_hbond.py

--- tests/NonBondSampling/afm_hbond.py
+++ tests/NonBondSampling/afm_hbond.py
@@ -51,6 +51,79 @@ def drawTriangles(tris, points, ax=None):
     poly_collection = PolyCollection(triangles, edgecolors='gray' )
     ax.add_collection(poly_collection)
 
+
+def bonder_edges(polygons, bonds, border_polys=None, border_atoms=None, neighs=None, np0=0 ):
+    if border_atoms is None:
+        is_capping   = [ (len(ngs)==1) for ngs in neighs ]
+        is_kink      = [ (len(ngs)==2) for ngs in neighs ]
+        border_atoms = np.logical_or( is_capping, is_kink )
+    if border_polys is None:
+        is_capping_b = [ (is_capping[b[0]] or is_capping[b[1]]) for b in bonds ]
+        border_polys = [ any( is_capping_b[ binds[edge] ] for edge in polygon) for polygon in polygons ]
+    edges = []
+    for ip,poly in enumerate(polygons):
+        if not border_polys[ip]: continue
+        for ib in poly:
+            (i,j) = bonds[ib]
+            if   border_atoms[i]:
+                edges.append( (i,ip+np0) )
+            elif border_atoms[j]:
+                edges.append( (j,ip+np0) )
+    return edges
+
+
+def order_contour(bonds, start=None ):
+    # Create a dictionary to store adjacency list
+    adjacency = {}
+    for i, j in bonds:
+        if i not in adjacency: adjacency[i] = []
+        if j not in adjacency: adjacency[j] = []
+        adjacency[i].append(j)
+        adjacency[j].append(i)
+    # Start from an arbitrary point
+    if start is None: start = bonds[0][0]
+    #contour = [start]
+    contour = []
+    current = start
+    prev    = None 
+    while True:
+        next_point = None
+        ngs = adjacency[current]
+        bFound = False
+        for ng in ngs:
+            if ng != prev:
+                prev       = current
+                current    = ng
+                bFound = True
+                break
+        if not bFound: break 
+        contour.append(current)
+        if current == start: break
+    return contour
+
+def offset_contour( controur, ps, l=2.0, cAve=1.0 ):
+    print("========")
+    n = len(controur)
+    p0 = np.average( ps, axis=0 )
+    ps2 = np.zeros( (n,2) ) 
+    for i in range(0, n ):
+        pp  = ps[ controur[i-2] ]
+        pi  = ps[ controur[i-1] ]
+        pm  = ps[ controur[i  ] ]
+        d1  = pp-pi;   d1/=np.sqrt( np.dot(d1,d1) )
+        d2  = pm-pi;   d2/=np.sqrt( np.dot(d2,d2) )
+        d   = d1 + d2; d /=np.sqrt( np.dot(d ,d) )
+        d0  = pi - p0; c=np.dot(d0,d)
+        if c<0: d*=-1
+        #print( i, pi )
+        ps2[i-1,:] = pi + d*l
+    if cAve > 0:
+        ps2 = (ps2[:,:] + cAve*np.roll(ps2,1,axis=0) + cAve*np.roll(ps2,-1,axis=0) )/(1. + 2.*cAve)
+    return ps2
+
+
+
+
 # ================== Body
 
 mol.findBonds()                                             #; print( "mol.bonds ", mol.bonds )
@@ -59,18 +132,47 @@ bonds        = mol.bonds
 binds        = np.repeat(np.arange(len(bonds)), 2)   ;print("binds ", binds )  # indexes of bonds for each point
 #bsamp       = au.makeBondSamples( bonds, mol.apos, where=[-0.4,0.0,0.4] )
 bsamp        = au.makeBondSamples( bonds, mol.apos, where=None )
-centers, cis = au.colapse_to_means( bsamp, R=0.7, binds=binds )
+centers, polygons = au.colapse_to_means( bsamp, R=0.7, binds=binds )
+
+polys = [  set( binds[p] for p in poly )  for poly in polygons ]
+
+print( "polys ", polys )
 
 #tris = skelet2triangles( bonds, b2c, npoint=len(mol.apos)+1 );   print("tris ", tris)
-tris = skelet2triangles2( cis, binds, bonds, npoint=len(mol.apos) );   print("tris ", tris)
+tris = skelet2triangles2( polygons, binds, bonds, npoint=len(mol.apos) ); #  print("tris ", tris)
 
-print( "mol.apos.shape, centers.shape ", mol.apos.shape, centers.shape  )
+#print( "mol.apos.shape, centers.shape ", mol.apos.shape, centers.shape  )
 points = np.concatenate( (mol.apos[:,:2], centers[:,:2]), axis=0 )
 
 plt.show()
 
 #exit()
 neighs = au.neigh_atoms( len(mol.enames), mol.bonds )
+
+is_capping   = [ (len(neighbors)==1) for neighbors in neighs ]
+is_kink      = [ (len(neighbors)==2) for neighbors in neighs ]
+is_capping_b = [ (is_capping[b[0]] or is_capping[b[1]]) for b in bonds ]
+border_atoms = np.logical_or( is_capping, is_kink )
+#is_peripheral = [ ((len(neighbors)==1) or (len(neighbors)==2))  for neighbors in neighs ]
+#is_per = [  for ci in  cis ]
+border_polys = [ any( is_capping_b[ binds[edge] ] for edge in polygon) for polygon in polygons ]
+
+
+edges = bonder_edges( polys, bonds, border_polys=border_polys, border_atoms=border_atoms, np0=len(mol.apos),  );  print( "edges ", edges )
+#edges = bonder_edges( polys, bonds, neighs=neighs, np0=len(mol.apos) );  print( "edges ", edges )
+
+contour = order_contour(edges) #;print( "contour ", contour )
+
+plt.plot( points[contour,0], points[contour,1], 'o:r', lw=3  )
+
+contour2  = offset_contour( contour, points, l=2.0 )     #;print( "contour2 ", contour2.shape, contour2  )
+
+plt.plot( contour2[:,0], contour2[:,1], 'o-b', lw=3  )
+
+#plu.plotBonds( links=edges, ps=points, lws=5, colors='r' )
+     
+
+
 
 asamp = au.makeAtomSamples    ( neighs, mol.apos, mol.enames,  ignore=set(['H']), where=[-0.6] )
 esamp = au.makeEndAtomSamples ( neighs, mol.apos, mol.enames, ignore=set(['H']),  whereX=[-0.6, +0.6 ], whereY=[0.0,+0.6] )
@@ -85,8 +187,15 @@ print( " tot npoint= ", nbsamp+nasamp+nesamp+nksamp ," nbsamp=", nbsamp," nasamp
 
 
 plt.plot( mol.apos[:,0],mol.apos[:,1], "o", ms=10, )
+
+drawTriangles(tris, points, ax=None)
+
 plt.plot( centers[:,0],centers[:,1], "o", ms=15. )
 #plt.plot( bsamp[:,0],bsamp[:,1], "." )
+
+
+plt.plot( mol.apos[border_atoms,0],mol.apos[border_atoms,1], "xm", ms=15. )
+plt.plot( centers[border_polys,0],centers[border_polys,1], "*m", ms=15. )
 
 #plt.plot( esamp[:,0],esamp[:,1], "." )
 #plt.plot( ksamp[:,0],ksamp[:,1], "." )
@@ -94,7 +203,7 @@ plt.plot( centers[:,0],centers[:,1], "o", ms=15. )
 
 
 
-drawTriangles(tris, points, ax=None)
+
 
 ax=plt.gca()
 ax.scatter(points[:, 0], points[:, 1], c='b', marker='o')    
