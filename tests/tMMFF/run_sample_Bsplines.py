80cd5d6b38a4b6fa1272076f3a38b0fdb092f66c
Prokop Hapala <ProkopHapala@gmail.com>
2024-07-25 17:00:40 +0200

    sketched test_fit_3D in /tests/tMMFF/run_sample_Bsplines.py

--- tests/tMMFF/run_sample_Bsplines.py
+++ tests/tMMFF/run_sample_Bsplines.py
@@ -21,16 +21,29 @@ REs=[
 
 # =============
 
+### ==== ToDo: Extract these functions to GridUtils.py (?)
+
 def getCos( xs, freq ):
     E =       np.cos( freq*xs )
     F = -freq*np.sin( freq*xs )
     return E,F
 
 def getCos2D( Xs, Ys, freq=(np.pi,np.pi) ):
-    E =       np.cos( freq[0]*Xs )*np.cos( freq[1]*Ys )
+    E =           np.cos( freq[0]*Xs )*np.cos( freq[1]*Ys )
     Fx = -freq[0]*np.sin( freq[0]*Xs )*np.cos( freq[1]*Ys )
     Fy = -freq[1]*np.cos( freq[0]*Xs )*np.sin( freq[1]*Ys )
     return E,Fx,Fy
+
+def getCos3D( Xs, Ys, Zs, freq=(np.pi,np.pi,np.pi) ):
+    fx,fy,fz = freq
+    cx = np.cos( fx*Xs );  sx = np.sin( fx*Xs )
+    cy = np.cos( fy*Ys );  sy = np.sin( fy*Ys )
+    cz = np.cos( fz*Zs );  sz = np.sin( fz*Zs )
+    E =           cx*cy*cz
+    Fx = -fx*sx*cy*cz
+    Fy = -fy*cx*sy*cz
+    Fz = -fz*cx*cy*sz
+    return E,Fx,Fy,Fz
 
 def getLJ( r, R0, E0 ):
     #r = np.sqrt(x**2 + y**2 + z**2)
@@ -61,6 +74,38 @@ def getLJ_atoms( apos, REs, Xs,Ys,Zs ):
         Fy += fr*dy/r
         Fz += fr*dz/r
     return Es, Fx,Fy,Fz
+
+def make2Dsampling(  g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1) ):
+    xs  = np.arange(g0[0], gmax[0], dg[0])
+    ys  = np.arange(g0[1], gmax[1], dg[0])
+    Xs,Ys = np.meshgrid(xs,ys)
+    return Xs,Ys
+
+def pack_ps2D( Xs, Ys):
+    ps = np.zeros( ( len(Xs.flat), 2) )
+    ps[:,0] = Xs.flat
+    ps[:,1] = Ys.flat
+    return ps
+
+def make2Dsampling_ps(  g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1) ):
+    Xs,Ys = make2Dsampling(  g0=g0, gmax=gmax, dg=dg )
+    sh = Xs.shape
+    ps = pack_ps2D( Xs, Ys)
+    return ps, sh
+
+def make3Dsampling(  g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1) ):
+    xs  = np.arange(g0[0], gmax[0], dg[0])
+    ys  = np.arange(g0[1], gmax[1], dg[0])
+    zs  = np.arange(g0[2], gmax[2], dg[2])
+    Xs,Ys,Zs = np.meshgrid(xs,ys,zs)
+    return Xs,Ys,Zs
+
+def pack_ps3D( Xs, Ys, Zs):
+    ps = np.zeros( ( len(Xs.flat), 3) )
+    ps[:,0] = Xs.flat
+    ps[:,1] = Ys.flat
+    ps[:,2] = Zs.flat
+    return ps
 
 def test_fit_1D( g0=2.0, gmax=10.0, dg=0.1, dsamp=0.02, bUseForce=True ):
     #x0 = 2.0
@@ -114,23 +159,6 @@ def test_fit_1D( g0=2.0, gmax=10.0, dg=0.1, dsamp=0.02, bUseForce=True ):
     plt.grid()
     plt.show()
 
-def make2Dsampling(  g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1) ):
-    xs  = np.arange(g0[0], gmax[0], dg[0])
-    ys  = np.arange(g0[1], gmax[1], dg[0])
-    Xs,Ys = np.meshgrid(xs,ys)
-    return Xs,Ys
-
-def pack_ps2D( Xs, Ys):
-    ps = np.zeros( ( len(Xs.flat), 2) )
-    ps[:,0] = Xs.flat
-    ps[:,1] = Ys.flat
-    return ps
-
-def make2Dsampling_ps(  g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1) ):
-    Xs,Ys = make2Dsampling(  g0=g0, gmax=gmax, dg=dg )
-    sh = Xs.shape
-    ps = pack_ps2D( Xs, Ys)
-    return ps, sh
 
 def test_fit_2D( g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1), dsamp=(0.05,0.05) ):
     #cmap="RdBu_r"
@@ -163,11 +191,9 @@ def test_fit_2D( g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1), dsamp=(0.05,0.05)
     Ecut = 100.0
 
     Gs, Ws = mmff.fit2D_Bspline( E, Ws=None, dt=0.4, nmaxiter=1000, Ftol=1e-7 )
-
-    Gmin = -np.abs(Gs).max()
-
     E_f = mmff.sample_Bspline2D( ps, Gs, g0, dg, fes=None  ).reshape(sh_samp+(3,))
 
+    Gmin = -np.abs(Gs).max()
     dG = Gs-E                    ; dGmin = -np.abs(dG).max()
     dE = E_f[:,:,2] - E_r[:,:]   ; dEmin = -np.abs(dE[4:-4,4:-4]).max()
     #dE = E_f[1:,1:,2] - E_r[:-1,:-1]   ; dEmin = -np.abs(dE).max()
@@ -184,31 +210,42 @@ def test_fit_2D( g0=(-5.0,2.0), gmax=(5.0,10.0), dg=(0.1,0.1), dsamp=(0.05,0.05)
     plt.subplot(2,3,5); plt.imshow( E_f[:,:,2], origin="lower", extent=extent, vmin=Emin, vmax=-Emin,   cmap=cmap ) ;plt.colorbar(); plt.title("E  fit")
     plt.subplot(2,3,6); plt.imshow( dE,         origin="lower", extent=extent, vmin=dEmin, vmax=-dEmin, cmap=cmap ) ;plt.colorbar(); plt.title("E(fit-ref)")
     plt.axis('equal')
-    '''
-    #Ws = Ecut/np.sqrt( E**2 + Ecut**2 )  ; EWs = E*Ws
-    Gs, Ws = mmff.fit_Bspline2D( E, Ws=None, dt=0.4, nmaxiter=1000, Ftol=1e-7 )
-    FEout = mmff.sample_Bspline( xs_, Gs, x0=g0, dx=dg )
-    plt.figure(figsize=(5,10))
-    plt.subplot(2,1,1)    
-    #plt.plot( xs, EWs, ".-k" )
-    plt.plot( xs, E, ".-k", label="E_ref" )
-    plt.plot( xs, Gs, ".-m", label="Gs" )
-    plt.plot( xs_, FEout[:,0], "-b", label="E_fit" )
-    #print( "Gs: ", Gs )
-    plt.ylim(Emin,-Emin)
-    plt.grid()
-    plt.legend()
-    plt.title("Energy")
-    plt.subplot(2,1,2)
-    plt.plot( xs,  -F , ".-k", label="F_ref" )    
-    plt.plot( xs_, -FEout[:,1], "-b", label="F_fit" )
-    plt.ylim(Fmin,-Fmin)
-    plt.legend()
-    plt.title("Force")
-    plt.grid()
-    plt.show()
-    '''
 
+def test_fit_3D( g0=(-5.0,-5.0,2.0), gmax=(5.0,-5.0,10.0), dg=(0.1,0.1,0.1), dsamp=(0.05,0.05,0.05) ):
+    cmap="bwr"
+    Xs,Ys,Ys    = make3Dsampling(  g0=g0, gmax=gmax, dg=dg )
+    Xs_,Ys_,Ys_ = make3Dsampling(  g0=g0, gmax=gmax, dg=dsamp )
+
+    Xs_*=0.999999; Ys_*=0.999999; Zs_*=0.999999;
+
+    sh_samp = Xs_.shape
+    ps      = pack_ps3D(Xs_,Ys_,Ys_)
+    #ps, sh_samp = make2Dsampling_ps(  g0=g0, gmax=gmax, dg=dsamp )
+    print( "Xs.shape ", Xs.shape )
+    #E, Fx,Fy,Fz = getLJ_atoms( apos, REs, Xs,Ys,Xs*0.0 )
+    E,  Fx,Fy      = getCos3D( Xs, Ys   )
+    E_r, Fx_r,Fy_r = getCos3D( Xs_, Ys_ )
+
+    Emin =  E.min()
+    Fmin = -Fy.max()
+
+    Gs, Ws = mmff.fit3D_Bspline( E, Ws=None, dt=0.4, nmaxiter=1000, Ftol=1e-7 )
+    E_f    = mmff.sample_Bspline3D( ps, Gs, g0, dg, fes=None  ).reshape(sh_samp+(3,))
+
+    Gmin = -np.abs(Gs).max()
+    dG = Gs-E                       ; dGmin = -np.abs(dG).max()
+    dE = E_f[:,:,:,2] - E_r[:,:,:]  ; dEmin = -np.abs(dE[4:-4,4:-4,4:-4]).max()
+
+    # extent=(g0[0],gmax[0],g0[1],gmax[1])
+    # plt.figure(figsize=(15,10))
+    # plt.subplot(2,3,1); plt.imshow( E,         origin="lower", extent=extent, vmin=Emin,  vmax=-Emin,  cmap=cmap ) ;plt.colorbar(); plt.title("Eg")
+    # plt.subplot(2,3,2); plt.imshow( Gs,        origin="lower", extent=extent, vmin=Gmin,  vmax=-Gmin,  cmap=cmap ) ;plt.colorbar(); plt.title("Gs fit")
+    # plt.subplot(2,3,3); plt.imshow( dG,        origin="lower", extent=extent, vmin=dGmin, vmax=-dGmin, cmap=cmap ) ;plt.colorbar(); plt.title("Gs-Eg")
+    
+    # plt.subplot(2,3,4); plt.imshow( E_r,        origin="lower", extent=extent, vmin=Emin, vmax=-Emin,   cmap=cmap ) ;plt.colorbar(); plt.title("E  ref")
+    # plt.subplot(2,3,5); plt.imshow( E_f[:,:,2], origin="lower", extent=extent, vmin=Emin, vmax=-Emin,   cmap=cmap ) ;plt.colorbar(); plt.title("E  fit")
+    # plt.subplot(2,3,6); plt.imshow( dE,         origin="lower", extent=extent, vmin=dEmin, vmax=-dEmin, cmap=cmap ) ;plt.colorbar(); plt.title("E(fit-ref)")
+    # plt.axis('equal')
 
 #mmff.setVerbosity( 2 )
 mmff.setVerbosity( 3 )
