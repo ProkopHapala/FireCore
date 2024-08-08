900e66235de0f2fc85558f521c7df0489f389b7b
Prokop Hapala <ProkopHapala@gmail.com>
2024-08-08 11:37:10 +0200

    run_sample_Hermite.py works well with Hermite-cubic-spline interpolation sample1D_deriv_comb2() and fe1Dcomb2() implemented in InterpolateTricubic.h; ToDo: 3D version (mixed with explicit derivative just in z-direction)

--- tests/tMMFF/run_sample_Hermite.py
+++ tests/tMMFF/run_sample_Hermite.py
@@ -29,28 +29,28 @@ def test_1D( func, funcS, g0=2.0, gmax=10.0, dg=0.2, dsamp=0.02, scErr=100.0, ti
     xs  = np.arange(g0, gmax+1e-8, dg)     ; ng=len(xs)
     xs_ = np.arange(g0, gmax+1e-8 -3*dg, dsamp)  ; nsamp=len(xs_)
     
-    E0 = 1.0
-    R0 = 3.5
-    a  = 1.7
-    E_,F_ = func( xs_, R0, E0, a )
+    #E0 = 1.0
+    #R0 = 3.5
+    #a  = 1.7
+    E_,F_ = func( xs_ )
     if   ( mode==1 ):   # simple Hermite using just values
-        E,F   = func( xs,  R0, E0, a )
+        E,F   = func( xs)
         Gs = E
         print( "xs ",xs )
         FEout = mmff.sample_SplineHermite( xs_, Gs, g0=g0, dg=dg )
     elif ( mode==2 ):   # simple Hermite using derivatives
-        E,F   = func( xs,  R0, E0, a )
+        E,F   = func( xs )
         Gs    = np.zeros( (ng,2) )
         Gs[:,0] = E
         Gs[:,1] = F
         FEout = mmff.sample_SplineHermite1D_deriv( xs_, Gs, g0=g0, dg=dg )
     elif ( mode==3 ):   # hermite using linear combination of splited function
-        Ep,El,Fp,Fl = funcS( xs, R0, E0, a )
+        Ep,El,Fp,Fl = funcS( xs )
         Gs = np.zeros( (ng,4) )
         Gs[:,0] = Ep
-        Gs[:,1] = Fp
+        Gs[:,1] = Fp*dg
         Gs[:,2] = El
-        Gs[:,3] = Fl
+        Gs[:,3] = Fl*dg
         FEout = mmff.sample_SplineHermite_comb( xs_, Gs, [1.0,1.0], ncomb=2, g0=g0, dg=dg )
 
     # ------ plotting
@@ -80,14 +80,22 @@ def test_1D( func, funcS, g0=2.0, gmax=10.0, dg=0.2, dsamp=0.02, scErr=100.0, ti
     #plt.show()
 
 
+R0 = 3.5
+E0 = 1.0
+a  = 1.8
 
-#test_1D( fu.getLJ, fu.getLJSplit, title="LJ simple", mode=1)
-#test_1D( fu.getLJ, fu.getLJSplit, title="LJ deriv", mode=2)
-#test_1D( fu.getLJ, fu.getLJSplit, title="LJ deriv split", mode=3)
 
-test_1D( fu.getMorse, fu.getMorseSplit, title="LJ simple", mode=1)
-#test_1D( fu.getMorse, fu.getMorseSplit, title="LJ deriv", mode=2)
-#test_1D( fu.getMorse, fu.getMorseSplit, title="LJ deriv split", mode=3)
+#test_1D( lambda x:fu.getLJ(x,R0,E0), lambda x:fu.getLJSplit(x,R0,E0), title="LJ simple",      mode=1 )
+#test_1D( lambda x:fu.getLJ(x,R0,E0), lambda x:fu.getLJSplit(x,R0,E0), title="LJ deriv",       mode=2 )
+#test_1D( lambda x:fu.getLJ(x,R0,E0), lambda x:fu.getLJSplit(x,R0,E0), title="LJ deriv split", mode=3 )
+
+test_1D( lambda x:fu.getMorse(x,R0,E0), lambda x:fu.getMorseSplit(x,R0,E0), title="Morse simple",      mode=1 )
+test_1D( lambda x:fu.getMorse(x,R0,E0), lambda x:fu.getMorseSplit(x,R0,E0), title="Morse deriv",       mode=2 )
+test_1D( lambda x:fu.getMorse(x,R0,E0), lambda x:fu.getMorseSplit(x,R0,E0), title="Morse deriv split", mode=3 )
+
+#fu.checkNumDeriv( lambda x: fu.getLJ(x,3.5,1.0),        2.0, 6.0, tol=1e-6, n=400, errSc=100.0, plt=plt, label="LJ"   ,c='r' )
+#fu.checkNumDeriv( lambda x: fu.getMorse(x,3.5,1.0,1.8), 2.0, 6.0, tol=1e-6, n=400, errSc=100.0, plt=plt, label="Morse",c='b' )
+
 
 #test_1D( title="No-Half", mode=1)
 #test_1D( title="No-Half", mode=1)
