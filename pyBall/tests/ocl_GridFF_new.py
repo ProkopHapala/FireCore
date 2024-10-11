ced5b12cf8a069a2f869248053e3ed4d705beaea
Prokop Hapala <ProkopHapala@gmail.com>
2024-10-11 20:22:37 +0200

    testing modified version of GridFF_cl::fit3D() from /pyBall/OCL/GridFF.py; it is not quite converging but otherwise works (does not crash), possibly the ns[x,y,z] dimensions are wrongly ordered? see test in tests/tMMFF/run_test_GridFF_ocl_new.py and test_gridFF_ocl() in /pyBall/tests/ocl_GridFF_new.py

--- pyBall/tests/ocl_GridFF_new.py
+++ pyBall/tests/ocl_GridFF_new.py
@@ -6,11 +6,21 @@ import matplotlib.pyplot as plt
 from .. import atomicUtils as au
 #from .. import FunctionSampling as fu
 from ..OCL.GridFF import GridFF_cl, GridShape
+from .Ewald import compute_potential, plot1Dcut
 
 # =============  Functions
 
+mmff = None
+
 os.environ['PYOPENCL_CTX'] = '0'
 clgff = GridFF_cl()
+
+def try_load_mmff():
+    global mmff
+    if mmff is None:
+        from .. import MMFF as mmff_
+        mmff = mmff_
+
 
 def autoPBC(lvec,Rcut=20.0,mask=(1,1,0)):
     nPBC = [0,0,0]
@@ -21,7 +31,81 @@ def autoPBC(lvec,Rcut=20.0,mask=(1,1,0)):
             nPBC[i] = int(Rcut/L)+1
     return tuple(nPBC)
 
-def test_gridFF_ocl( fname="./data/xyz/NaCl_1x1_L1.xyz", Element_Types_name="./data/ElementTypes.dat", bSymetrize=False, mode=6, dsamp=0.02, p0=[0.0,0.0,2.0], R0=3.5, E0=0.1, a=1.6, Q=0.4, H=0.0, scErr=100.0, iax=2, Emax=None, Fmax=None, maxSc=5.0, title=None, bSaveFig=True, bRefine=True, nPBC=[100,100,0], bRealSpace=False ):
+def test_Ewald( apos, qs, ns=[100,100,100], dg=(0.1,0.1,0.1), nPBC=[30,30,30], pos0=None, scErr=100.0, order=2, bPython=True, bOCL=True, bPlotPy=False,  bPlotOcl=True, bOMP=False, nBlur=0, cSOR=0.0, cV=0.5, yrange=None, bPlot1D=True , bSlab=False, z_slab=None ):
+    try_load_mmff()
+
+    apos = apos.copy()
+    print( "apos ", apos )
+    Ls = [ ns[0]*dg[0], ns[1]*dg[1], ns[2]*dg[2] ]                                # lenghts along each axis  
+    if pos0 is None: pos0=np.array( [ -0.5*Ls[0], -0.5*Ls[1], -0.5*Ls[2] ] )                       # origin of grid - half of the grid size
+    mmff.setupEwaldGrid( ns, dg=dg, pos0=pos0 )                                   # setup grid in C++ for projection
+    dens = mmff.projectAtomsEwaldGrid( apos, qs, ns=ns, order=order )             # project atoms to grid using C++ 
+
+    # perhaps we should find better reference than direct sum in reals space
+    #  * perhaps using [Madelung Constant](https://en.wikipedia.org/wiki/Madelung_constant) for some known crystals
+
+    Qtot = np.abs(dens).sum();        print("Qtot = ", Qtot, np.abs(qs).sum() )   # check if the total charge is conserved in projection ( should be same as sum of qs )
+    
+    # set voxel through which we cut the 3D grid Vg in debug plots
+    ix0=ns[0]//2
+    iy0=ns[1]//2
+    iz0=ns[2]//2
+    i0 = (ix0,iy0,iz0)
+
+    if bPython:
+        Vg,(density_fft, Vw, ker_w) = compute_potential(dens, dg, bInternals=True )
+
+        if bPlotPy:
+            # dens2d =  np.sum( dens, axis=iax )
+            plt.figure(figsize=(15,10))
+            extent  =[  -Ls[0]*0.5  , +Ls[0]*0.5,    -Ls[1]*0.5  ,+Ls[1]*0.5      ]
+            extent_w=[  -np.pi/dg[0], +np.pi/dg[0],  -np.pi/dg[1],+np.pi/dg[1]    ]
+            # --- plot in real-space (cartesian) coordinates
+            plt.subplot(2,3,1  ); plt.imshow( dens [iz0,:,:],                        cmap='bwr', extent=extent  , origin="lower" ); plt.colorbar(); plt.title("Charge Density" )
+            plt.subplot(2,3,2  ); plt.imshow( ker_w[iz0,:,:],                        cmap='bwr', extent=extent_w, origin="lower" ); plt.colorbar(); plt.title("ker_w" )
+            plt.subplot(2,3,3  ); plt.imshow( Vg   [iz0,:,:],   vmin=-1.0,vmax=1.0,  cmap='bwr', extent=extent  , origin="lower" ); plt.colorbar(); plt.title("V ewald " )
+            # --- plot in pixel-coordinates
+            plt.subplot(2,3,3+1); plt.imshow( dens [iz0,:,:],                        cmap='bwr'               , origin="lower" ); plt.colorbar(); plt.title("Charge Density" )
+            plt.subplot(2,3,3+2); plt.imshow( ker_w[iz0,:,:],                        cmap='bwr'               , origin="lower" ); plt.colorbar(); plt.title("ker_w" )
+            plt.subplot(2,3,3+3); plt.imshow( Vg   [iz0,:,:],   vmin=-1.0,vmax=1.0,  cmap='bwr'               , origin="lower" ); plt.colorbar(); plt.title("V ewald" )
+    
+    if bOCL:
+        grid = GridShape( dg=dg,  Ls=Ls, g0=tuple(pos0) )
+        clgff.set_grid( grid )
+        xyzq = np.zeros( (len(apos),4), dtype=np.float32 )
+        xyzq[:,0] = apos[:,0]
+        xyzq[:,1] = apos[:,1]
+        xyzq[:,2] = apos[:,2]
+        xyzq[:,3] = qs
+        Vocl = clgff.makeCoulombEwald( xyzq )
+
+        print( "Vocl min,max", Vocl.min(), Vocl.max() )
+
+        if bPlotOcl:
+            # dens2d =  np.sum( dens, axis=iax )
+            plt.figure(figsize=(10,5))
+            extent  =[  -Ls[0]*0.5  , +Ls[0]*0.5,    -Ls[1]*0.5  ,+Ls[1]*0.5      ]
+            plt.subplot(1,2,1); plt.imshow( Vocl[iz0,:,:],   vmin=-1.0,vmax=1.0,  cmap='bwr', extent=extent  , origin="lower" ); plt.colorbar(); plt.title("V ewald " )
+            plt.subplot(1,2,2); plt.imshow( Vocl[:,iy0,:],   vmin=-1.0,vmax=1.0,  cmap='bwr'                 , origin="lower" ); plt.colorbar(); plt.title("V ewald" )
+
+    # ---- 1D debug plots
+    if bPlot1D:
+        plt.figure(figsize=(15,5))
+        plt.subplot(1,3,1); plot1Dcut( apos, qs, Vocl, Vref=Vg, i0=i0, dg=dg, Ls=Ls, iax=0, nPBC=nPBC, scErr=scErr )
+        plt.subplot(1,3,2); plot1Dcut( apos, qs, Vocl, Vref=Vg, i0=i0, dg=dg, Ls=Ls, iax=1, nPBC=nPBC, scErr=scErr )
+        plt.subplot(1,3,3); plot1Dcut( apos, qs, Vocl, Vref=Vg, i0=i0, dg=dg, Ls=Ls, iax=2, nPBC=nPBC, scErr=scErr )
+        #plt.subplot(1,3,3); plot1Dcut( Vg, i0=i0, dg=dg, lvec=lvec, iax=2, nPBC=nPBC, scErr=scErr )
+        if yrange is not None:
+            plt.subplot(1,3,1); plt.ylim(yrange)
+            plt.subplot(1,3,2); plt.ylim(yrange)
+            plt.subplot(1,3,3); plt.ylim(yrange)
+        #plt.tight_layout()
+        #plt.show()
+        #super title
+        plt.suptitle( "order=" + str(order) + " nBlur=" + str(nBlur) + " cSOR=" + str(cSOR) + " cV=" + str(cV) )
+
+
+def test_gridFF_ocl( fname="./data/xyz/NaCl_1x1_L1.xyz", Element_Types_name="./data/ElementTypes.dat", bSymetrize=False, bMorse=True, bEwald=False, bFit=True,  mode=6, dsamp=0.02, p0=[0.0,0.0,2.0], R0=3.5, E0=0.1, a=1.6, Q=0.4, H=0.0, scErr=100.0, iax=2, Emax=None, Fmax=None, maxSc=5.0, title=None, bSaveFig=True, bRefine=True, nPBC=[100,100,0], bRealSpace=False ):
     print( "py======= test_gridFF() START" );
 
     #Element_Types_name="/home/prokop/git/FireCore/tests/tMMFF/data/ElementTypes.dat"
@@ -50,87 +134,93 @@ def test_gridFF_ocl( fname="./data/xyz/NaCl_1x1_L1.xyz", Element_Types_name="./d
     REQs[:,2]  = atoms.qs
     REQs[:,3]  = 0.0
 
+    if bEwald:
+        #---- Test Charge-to-grid projection
+        """
+        #xyzq[:,2]=0.0
 
-    #---- Test Poisson
+        Qgrid = clgff.project_atoms_on_grid_quintic_pbc(xyzq, dg=(0.1,0.1,0.1),  lvec=atoms.lvec )
+
+        plt.figure( figsize=(25,5) )
+        plt.subplot(1,6,1); plt.imshow( Qgrid[166,:,:], cmap='bwr' ); plt.colorbar()
+        plt.subplot(1,6,2); plt.imshow( Qgrid[167,:,:], cmap='bwr' ); plt.colorbar()
+        plt.subplot(1,6,3); plt.imshow( Qgrid[168,:,:], cmap='bwr' ); plt.colorbar()
+        plt.subplot(1,6,4); plt.imshow( Qgrid[169,:,:], cmap='bwr' ); plt.colorbar()
+        plt.subplot(1,6,5); plt.imshow( Qgrid[170,:,:], cmap='bwr' ); plt.colorbar()
+        plt.subplot(1,6,6); plt.imshow( Qgrid[171,:,:], cmap='bwr' ); plt.colorbar()
+        #plt.subplot(1,6,6); plt.imshow( Qgrid[172,:,:], cmap='bwr' ); plt.colorbar()
+
+        #print( Qgrid[:,0,0], Qgrid[0,:,0], Qgrid[0,0,:], )
+        # for iz in range(10):
+        #     plt.figure()
+        #     iiz=iz*3
+        #     plt.imshow( Qgrid[:,:,iiz] )
+        #     plt.title( f"Qgrid[{iz}] {iz*3}" )
+
+        return
+        """
+
+        #---- Test Poisson
+        
+        grid = GridShape( dg=(0.1,0.1,0.1),  lvec=atoms.lvec)
+        clgff.set_grid( grid )
+        Vgrid = clgff.makeCoulombEwald( xyzq )
+
+        # xline = Vgrid.sum(axis=(1,2)); plt.plot( xline )
+        # yline = Vgrid.sum(axis=(0,2)); plt.plot( yline )
+        # zline = Vgrid.sum(axis=(0,1)); plt.plot( zline )
+
+        # plt.figure( figsize=(25,5) )
+        # plt.subplot(1,6,1); plt.imshow( Vgrid[166,:,:], cmap='bwr' ); plt.colorbar()
+        # plt.subplot(1,6,2); plt.imshow( Vgrid[167,:,:], cmap='bwr' ); plt.colorbar()
+        # plt.subplot(1,6,3); plt.imshow( Vgrid[168,:,:], cmap='bwr' ); plt.colorbar()
+        # plt.subplot(1,6,4); plt.imshow( Vgrid[169,:,:], cmap='bwr' ); plt.colorbar()
+        # plt.subplot(1,6,5); plt.imshow( Vgrid[170,:,:], cmap='bwr' ); plt.colorbar()
+        # plt.subplot(1,6,6); plt.imshow( Vgrid[171,:,:], cmap='bwr' ); plt.colorbar()
+
+        plt.figure( figsize=(15,5) )
+        plt.subplot(1,3,1); plt.imshow( Vgrid[170,:,:], cmap='bwr' ); plt.colorbar(); plt.title( "Vgrid[170,:,:]" );
+        plt.subplot(1,3,2); plt.imshow( Vgrid[:,0,:  ], cmap='bwr' ); plt.colorbar(); plt.title( "Vgrid[:,0,:  ]" );
+        plt.subplot(1,3,3); plt.imshow( Vgrid[:,:,0  ], cmap='bwr' ); plt.colorbar(); plt.title( "Vgrid[:,:,0  ]" );
     
-    grid = GridShape( dg=(0.1,0.1,0.1),  lvec=atoms.lvec)
-    clgff.set_grid( grid )
-    Vgrid = clgff.makeCoulombEwald( xyzq )
+    if bMorse:
+        nPBC = autoPBC(atoms.lvec,Rcut=20.0); print("autoPBC: ", nPBC )
 
-    # xline = Vgrid.sum(axis=(1,2)); plt.plot( xline )
-    # yline = Vgrid.sum(axis=(0,2)); plt.plot( yline )
-    # zline = Vgrid.sum(axis=(0,1)); plt.plot( zline )
 
-    # plt.figure( figsize=(25,5) )
-    # plt.subplot(1,6,1); plt.imshow( Vgrid[166,:,:], cmap='bwr' ); plt.colorbar()
-    # plt.subplot(1,6,2); plt.imshow( Vgrid[167,:,:], cmap='bwr' ); plt.colorbar()
-    # plt.subplot(1,6,3); plt.imshow( Vgrid[168,:,:], cmap='bwr' ); plt.colorbar()
-    # plt.subplot(1,6,4); plt.imshow( Vgrid[169,:,:], cmap='bwr' ); plt.colorbar()
-    # plt.subplot(1,6,5); plt.imshow( Vgrid[170,:,:], cmap='bwr' ); plt.colorbar()
-    # plt.subplot(1,6,6); plt.imshow( Vgrid[171,:,:], cmap='bwr' ); plt.colorbar()
+        if bFit:
+            clgff.make_MorseFF( xyzq, REQs, nPBC=nPBC, lvec=atoms.lvec, g0=(0.0,0.0,0.0), GFFParams=(0.1,1.5,0.0,0.0), bReturn=False )
+            V_Paul = clgff.fit3D( clgff.V_Paul_buff  )
+            V_Lond = clgff.fit3D( clgff.V_Lond_buff )
 
-    plt.figure( figsize=(15,5) )
-    plt.subplot(1,3,1); plt.imshow( Vgrid[170,:,:], cmap='bwr' ); plt.colorbar(); plt.title( "Vgrid[170,:,:]" );
-    plt.subplot(1,3,2); plt.imshow( Vgrid[:,0,:  ], cmap='bwr' ); plt.colorbar(); plt.title( "Vgrid[:,0,:  ]" );
-    plt.subplot(1,3,3); plt.imshow( Vgrid[:,:,0  ], cmap='bwr' ); plt.colorbar(); plt.title( "Vgrid[:,:,0  ]" );
+        else:
 
-    return
-    
-    #---- Test Charge-to-grid projection
-    """
-    #xyzq[:,2]=0.0
+            V_Paul, V_Lond = clgff.make_MorseFF( xyzq, REQs, nPBC=nPBC, lvec=atoms.lvec, g0=(0.0,0.0,0.0), GFFParams=(0.1,1.5,0.0,0.0)  )
 
-    Qgrid = clgff.project_atoms_on_grid_quintic_pbc(xyzq, dg=(0.1,0.1,0.1),  lvec=atoms.lvec )
+            print( "V_Paul.shape ", V_Paul.shape )
+            plt.figure()
+            plt.plot( V_Paul[:,0,0], label="V_Paul(0,0,z)" )
+            plt.plot( V_Lond[:,0,0], label="V_Lond(0,0,z)" )
+            plt.plot( V_Paul[:,20,20], label="V_Paul(20,20,z)" )
+            plt.plot( V_Lond[:,20,20], label="V_Lond(20,20,z)" )
+            plt.legend()
+            #plt.yscale('log')
+            plt.grid()
 
-    plt.figure( figsize=(25,5) )
-    plt.subplot(1,6,1); plt.imshow( Qgrid[166,:,:], cmap='bwr' ); plt.colorbar()
-    plt.subplot(1,6,2); plt.imshow( Qgrid[167,:,:], cmap='bwr' ); plt.colorbar()
-    plt.subplot(1,6,3); plt.imshow( Qgrid[168,:,:], cmap='bwr' ); plt.colorbar()
-    plt.subplot(1,6,4); plt.imshow( Qgrid[169,:,:], cmap='bwr' ); plt.colorbar()
-    plt.subplot(1,6,5); plt.imshow( Qgrid[170,:,:], cmap='bwr' ); plt.colorbar()
-    plt.subplot(1,6,6); plt.imshow( Qgrid[171,:,:], cmap='bwr' ); plt.colorbar()
-    #plt.subplot(1,6,6); plt.imshow( Qgrid[172,:,:], cmap='bwr' ); plt.colorbar()
-
-    #print( Qgrid[:,0,0], Qgrid[0,:,0], Qgrid[0,0,:], )
-    # for iz in range(10):
-    #     plt.figure()
-    #     iiz=iz*3
-    #     plt.imshow( Qgrid[:,:,iiz] )
-    #     plt.title( f"Qgrid[{iz}] {iz*3}" )
-
-    return
-    """
-    #---- Test Morse
-    """
-    nPBC = autoPBC(atoms.lvec,Rcut=20.0); print("autoPBC: ", nPBC )
-
-    V_Paul, V_Lond = clgff.make_MorseFF( xyzq, REQs, nPBC=nPBC, lvec=atoms.lvec, g0=(0.0,0.0,0.0), GFFParams=(0.1,1.5,0.0,0.0)  )
-
-    print( "V_Paul.shape ", V_Paul.shape )
-    plt.figure()
-    plt.plot( V_Paul[:,0,0], label="V_Paul(0,0,z)" )
-    plt.plot( V_Lond[:,0,0], label="V_Lond(0,0,z)" )
-    plt.plot( V_Paul[:,20,20], label="V_Paul(20,20,z)" )
-    plt.plot( V_Lond[:,20,20], label="V_Lond(20,20,z)" )
-    plt.legend()
-    #plt.yscale('log')
-    plt.grid()
-
-    ix=0
-    iy=0
-    iz=5
-    replicate_count = 3  # Number of replications
-    V_Paul_cut = V_Paul[iz,iy,:]; V_Paul_cut_rep = np.tile(V_Paul_cut, replicate_count)
-    V_Lond_cut = V_Lond[iz,iy,:]; V_Lond_cut_rep = np.tile(V_Lond_cut, replicate_count)
-    
-    plt.figure()
-    # Plot the replicated 1D cuts
-    plt.plot( V_Paul_cut_rep, label=f"V_Paul(x,{iy},{iz}) - replicated")
-    plt.plot( V_Lond_cut_rep, label=f"V_Lond(x,{iy},{iz}) - replicated")
-    plt.legend()
-    plt.grid()
-    plt.show()
-    plt.show()
-    """
+            ix=0
+            iy=0
+            iz=5
+            replicate_count = 3  # Number of replications
+            V_Paul_cut = V_Paul[iz,iy,:]; V_Paul_cut_rep = np.tile(V_Paul_cut, replicate_count)
+            V_Lond_cut = V_Lond[iz,iy,:]; V_Lond_cut_rep = np.tile(V_Lond_cut, replicate_count)
+            
+            plt.figure()
+            # Plot the replicated 1D cuts
+            plt.plot( V_Paul_cut_rep, label=f"V_Paul(x,{iy},{iz}) - replicated")
+            plt.plot( V_Lond_cut_rep, label=f"V_Lond(x,{iy},{iz}) - replicated")
+            plt.legend()
+            plt.grid()
+            plt.show()
+            plt.show()
+        
     print( "py======= test_gridFF() DONE" );
 
