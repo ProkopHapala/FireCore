10d7f899adf752276655222bb257cc81b74cd1b0
ProkopHapala <prokophapala@gmail.com>
2024-10-14 18:33:19 +0200

    modified to chose proper device on laptop; tested that both Morse and Ewald ing GridFF.py works on GPU

--- tests/tMMFF/run_test_GridFF_ocl_new.py
+++ tests/tMMFF/run_test_GridFF_ocl_new.py
@@ -16,7 +16,7 @@ Q = 0.4
 p0 = [-2.0,-2.0,0.0]
 
 #mol_name="PTCDA.xyz"
-gff.test_gridFF_ocl( fname="data/xyz/NaCl_1x1_L2.xyz" )
+#gff.test_gridFF_ocl( fname="data/xyz/NaCl_1x1_L2.xyz" )
 #gff.test_gridFF_ocl( fname="/home/prokop/git/FireCore/tests/pyutils/NaCl_8x8_L3.xyz" )
 
 # ======== Ewald
@@ -37,7 +37,7 @@ qs = [ +1.,+1.,-1.,-1. ]
 # ])
 # qs = [ +1.,-1. ]
 
-#gff.test_Ewald( apos, qs,  ns=(100,100,100), dg=(0.10,0.10,0.10), order=3, bSlab=True,  nPBC=(100,100,0) )
+gff.test_Ewald( apos, qs,  ns=(100,100,100), dg=(0.10,0.10,0.10), order=3, bSlab=True,  nPBC=(100,100,0) )
 # gff.test_Ewald( apos, qs,  ns=[100,100,150], dg=[0.10,0.10,0.10], order=3, bSlab=True,  nPBC=[100,100,0] )
 # gff.test_Ewald( apos, qs,  ns=[100,100,200], dg=[0.10,0.10,0.10], order=3, bSlab=True,  nPBC=[100,100,0] )
 # gff.test_Ewald( apos, qs,  ns=[100,100,300], dg=[0.10,0.10,0.10], order=3, bSlab=True,  nPBC=[100,100,0] )
