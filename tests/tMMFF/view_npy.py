1c8272331180901e01aae0ed6b45a3344060dd7c
ProkopHapala <ProkopHapala@gmail.com>
2024-11-07 17:39:59 +0100

    testing Bspline_PLQd.npy genrated for NaCl_1x1_L3.xyz vs NaCl_8x8_L3.xyz; The problem is that potential from NaCl_8x8_L3 are much bigger (for unknown reason), and as a result PTCDA molecule collapse into the substrate in MolGUIapp

--- tests/tMMFF/view_npy.py
+++ tests/tMMFF/view_npy.py
@@ -1,41 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import sys
+sys.path.append("../../")
+from pyBall import grid_utils as gu
+
 # ========= Functions
 
-def cut(dat,f=[0.8,0.5,0.5],iax=0, n=1, bPlot=True, label="", ls='-', lw='0.5'):
-    sh = dat.shape
-    if (   iax==0 ):
-        ys = dat[:,int(sh[1]*f[1]),int(sh[2]*f[2])]
-    elif ( iax==1 ):
-        ys = dat[int(sh[0]*f[0]),:,int(sh[2]*f[2])]
-    elif( iax==2 ):
-        ys = dat[int(sh[0]*f[0]),int(sh[1]*f[1]),:]
-    if n>1: ys = np.tile( ys, n )
-    if bPlot:  plt.plot( ys, ls, lw=lw, label=label+str(iax)+" f="+str(f) )
-    return ys
-
-def cut2(dat,f=0.5,iax=0, n=[1,1], bPlot=True, vmax=0.1 ):
-    ni = dat.shape[iax]
-    if ( iax==0 ):
-        ys = dat[int(ni*f),:,:]
-    elif ( iax==1 ):
-        ys = dat[:,int(ni*f),:]
-    elif( iax==2 ):
-        ys = dat[:,:,int(ni*f)]
-    #print("ys.shape ", iax, ys.shape)
-    if (n[0]>1) or (n[1]>1): ys = np.tile( ys, n )
-    if bPlot: plt.imshow( ys, origin='lower', vmin=-vmax,vmax=vmax, cmap='bwr' )
-    return ys
-
-def cut1d_xyz( dat, f=[0.8,0.5,0.5],n=2, iaxs=[0,1,2], label=""):
-    for iax in iaxs: cut(dat,f=f,iax=iax, n=n, bPlot=True, label=label)
-    # cut(dat,f=f,iax=0, n=n, bPlot=True )
-    # cut(dat,f=f,iax=1, n=n, bPlot=True)
-    # cut(dat,f=f,iax=2, n=n, bPlot=True)
-    plt.legend()
-    plt.grid()
-    plt.title(name)    
 
 # ========= MAIN
 
@@ -64,12 +35,10 @@ path="data/NaCl_1x1_L2/"
 #     print( "name ", name, dat.shape," vmax=", vmax )
 #     #plt.figure(fig1.number); plt.subplot(2,3,i+1);  cut1d_xyz(dat, f=f, n=2 )
 #     plt.figure(fig1.number); plt.subplot(2,3,i+1);  
-#     cut1d_xyz(dat, f=f , n=2, iaxs=[1,2] )
-#     cut1d_xyz(dat, f=f2, n=2, iaxs=[1,2] )
-#     cut1d_xyz(dat, f=f3, n=2, iaxs=[1,2] )
-#     #plt.figure(fig2.number); plt.subplot(2,3,i+1);  cut2(dat,f=f[iax],iax=iax, n=[0,0], bPlot=True); plt.title(name)  
-
-
+#     gu.cut1d_xyz(dat, f=f , n=2, iaxs=[1,2] )
+#     gu.cut1d_xyz(dat, f=f2, n=2, iaxs=[1,2] )
+#     gu.cut1d_xyz(dat, f=f3, n=2, iaxs=[1,2] )
+#     #plt.figure(fig2.number); plt.subplot(2,3,i+1);  gu.cut2(dat,f=f[iax],iax=iax, n=[0,0], bPlot=True); plt.title(name)  
 
 fname= "debug_VCoul_pbc.npy"
 dat = np.load(path+fname)
