e36881dc7d97d7702ae0fc02db119d3ea3126ff5
Milan Koci <kocimil1@fjfi.cvut.cz>
2024-12-04 10:56:57 +0100

    Prepare yml, Cmake and simple tests to CI

---
+++ cpp/tests/test_add.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+double add(double a, double b) {
+    return a + b;
+}
+
+TEST(AddFunctionTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(add(4, 3), 7);
+}
+
+// TEST(AddFunctionTest, HandlesNegativeNumbers) {
+//     EXPECT_EQ(add(-1, 1), 0) << "-1 + 1 is not 0?";
+// }
+
+// TEST(AddFunctionTest, HandlesZero) {
+//     EXPECT_EQ(add(0, 0), 0) << "Expected 5, but got " << add(0, 0);
+// }
+
+// TEST(AddFunctionTest, HandlesDecimalNumbers) {
+//     EXPECT_DOUBLE_EQ(add(2.5, 3.5), 6.0);
+// }
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ cpp/tests/test_add.py
@@ -0,0 +1,6 @@
+def add(i,j):
+	return i+j
+
+
+def test_add():
+	assert add(2,3) == 5
