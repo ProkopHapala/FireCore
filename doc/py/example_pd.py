b2f9c2dbcc7c9103063abc3b2dd64af84c132ab8
ProkopHapala <prokophapala@gmail.com>
2024-12-21 18:08:33 +0100

    improved plotting example_pd.py

--- doc/py/example_pd.py
+++ doc/py/example_pd.py
@@ -1,7 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from projective_dynamics import build_grid_2d, solve_pd
+
+def plot_truss(points1, bonds, c='k', label=''   ):
+    """Plot two configurations of the grid for comparison"""
+    plt.plot(points1[:, 0], points1[:, 1], 'o', c=c, label=label)
+    for bond in bonds:
+        i, j = bond
+        plt.plot([points1[i, 0], points1[j, 0]], [points1[i, 1], points1[j, 1]], '-', c=c, alpha=0.3)
+    
+    plt.grid(True)
+    plt.axis('equal')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
 
 # Create a simple grid
 nx, ny = 5, 5
@@ -9,31 +21,10 @@ bonds, points, masses, ks, fixed = build_grid_2d(nx, ny, m=1.0, m_end=1000.0, l=
 velocity = np.zeros_like(points)
 
 # Run simulation
-new_points, new_velocity = solve_pd(points, velocity, bonds, masses, ks, 
-                                  dt=0.01, n_iter=100, fixed_points=fixed)
+new_points, new_velocity = solve_pd(points, velocity, bonds, masses, ks, dt=0.01, n_iter=100, fixed_points=fixed)
 
 # Visualize results
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111, projection='3d')
-
-# Plot initial configuration
-ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='blue', label='Initial')
-for bond in bonds:
-    i, j = bond
-    ax.plot([points[i, 0], points[j, 0]], 
-            [points[i, 1], points[j, 1]], 
-            [points[i, 2], points[j, 2]], 'b-', alpha=0.3)
-
-# Plot final configuration
-ax.scatter(new_points[:, 0], new_points[:, 1], new_points[:, 2], c='red', label='Final')
-for bond in bonds:
-    i, j = bond
-    ax.plot([new_points[i, 0], new_points[j, 0]], 
-            [new_points[i, 1], new_points[j, 1]], 
-            [new_points[i, 2], new_points[j, 2]], 'r-', alpha=0.3)
-
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.legend()
+plt.figure(figsize=(10, 10))
+plot_truss(points,     bonds, c='b', label='Initial Points' )
+plot_truss(new_points, bonds, c='r', label='Final Points' )
 plt.show()
