c502b173a3323e39e4cc6d0d9cd55d32dd4033bd
ProkopHapala <ProkopHapala@gmail.com>
2025-02-11 11:35:23 +0100

    rationalized AtomicSystem. merge_arrays filter_system and delete_atoms in atomicUtils.py

--- tests/tAttach/attach_new.py
+++ tests/tAttach/attach_new.py
@@ -34,7 +34,8 @@ from pyBall.atomicUtils import AtomicSystem
 #backbone = AtomicSystem(fname='B2.mol2')
 #endgroup = AtomicSystem(fname='G1.mol2')
 
-backbone  = AtomicSystem(fname='./backbones/PNA_sat-cis-.mol2')
+#backbone  = AtomicSystem(fname='./backbones/PNA_sat-cis-.mol2')
+backbone  = AtomicSystem(fname='./backbones/diacetylene-.mol2')
 endgroup1 = AtomicSystem(fname='./endgroups/guanine-.mol2')
 endgroup2 = AtomicSystem(fname='./endgroups/cytosine-.mol2')
 
@@ -42,8 +43,7 @@ backbone.preinitialize_atomic_properties()
 endgroup1.preinitialize_atomic_properties()
 endgroup2.preinitialize_atomic_properties()
 
-
-print("\nBackbone:"     ); backbone.print()
+#print("\nBackbone:"     ); backbone.print()
 #print("Backbone bonds:" ); backbone.printBonds()
 #print("Backbone neighs:"); backbone.printNeighs()
 #print("Endgroup:"); endgroup.print()
@@ -58,13 +58,15 @@ endgroup1.save_mol2('endgroup1.mol2')
 
 backbone.attach_group_by_marker(endgroup1, markerX="S_3", markerY="F",  _0=1)
 
+print( "saving subs 1 to files: backbone_subs_1.mol2, backbone_subs_1.xyz" )
 backbone.save_mol2('backbone_subs_1.mol2')
 backbone.saveXYZ('backbone_subs_1.xyz')
 
-print("Backbone neighs:"); backbone.printNeighs()
+#print("Backbone neighs:"); backbone.printNeighs()
 
 backbone.attach_group_by_marker(endgroup2, markerX="Se" , markerY="Cl", _0=1)
 
+print( "saving subs 2 to files: backbone_subs_2.mol2, backbone_subs_2.xyz" )
 backbone.save_mol2('backbone_subs_2.mol2')
 backbone.saveXYZ('backbone_subs_2.xyz')
 
