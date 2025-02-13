c09f1753e0d580622543605460fdde28c56c5428
ProkopHapala <ProkopHapala@gmail.com>
2025-02-13 12:34:13 +0100

    added files plot_EA.py and plot_EE.py to tEFF to plot 2D map of electron-electron and electron-ion interaction energy using eFF.sample_AE() and eFF.sample_ee()

--- tests/tEFF/run.sh
+++ tests/tEFF/run.sh
@@ -12,12 +12,16 @@ make eFF_lib
 cd $wd
 
 # ------- asan (Memory Sanitizer)
-#LD_PRELOAD=$(g++ -print-file-name=libasan.so)
-#echo   $LD_PRELOAD
-#export LD_PRELOAD
+LD_PRELOAD=$(g++ -print-file-name=libasan.so)
+echo   $LD_PRELOAD
+export LD_PRELOAD
 
 echo "#=========== RUN"
-python3 run_tests.py 2> ERR
+
+#python3 plot_EA.py 2> ERR | tee OUT
+python3 plot_EE.py 2> ERR | tee OUT
+
+#python3 run_tests.py 2> ERR
 #python3 run_dynamics.py 
 
 #echo "#=========== compare_components.py"
