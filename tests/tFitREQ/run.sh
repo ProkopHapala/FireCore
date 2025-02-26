59861dc84a44d7b77b2a9a0c0c2ec1d0d9b52d03
ProkopHapala <ProkopHapala@gmail.com>
2025-02-26 15:44:09 +0100

    debugging Buckts.h to properly use Groups (AABB bounding boxes) in NBFF

--- tests/tFitREQ/run.sh
+++ tests/tFitREQ/run.sh
@@ -12,9 +12,9 @@ make -j4 $name
 cd $wd
 
 # ------- asan (Memory Sanitizer)
-#LD_PRELOAD=$(g++ -print-file-name=libasan.so)
-#echo   $LD_PRELOAD
-#export LD_PRELOAD
+LD_PRELOAD=$(g++ -print-file-name=libasan.so)
+echo   $LD_PRELOAD
+export LD_PRELOAD
 
 #> FitREQ_debug.xyz
 #python3 run.py
