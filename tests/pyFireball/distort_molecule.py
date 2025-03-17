b8214e8d0146048cdcee5209a8323eb8a3b9a337
ProkopHapala <ProkopHapala@gmail.com>
2025-03-17 19:09:11 +0100

    scan_angle_dist in distort_molecule.py now does 2d scan of energy of H2O molecule with fireball

--- tests/pyFireball/distort_molecule.py
+++ tests/pyFireball/distort_molecule.py
@@ -1,10 +1,12 @@
 import sys
 import os
 import numpy as np
+np.set_printoptions(linewidth=1000)
 
 sys.path.append("../../")
 from pyBall import atomicUtils as au
 from pyBall.AtomicSystem import AtomicSystem
+from pyBall import FireCore as fc
 
 
 # ======== Body
@@ -61,7 +63,7 @@ def scan_angle( mol, angs, ia=0, j=1, jbs=[2] ):
             mol.apos[j,:] = mol.apos[ia,:] + ax*(r*ca) + vs[jj]*(r*sa)
         mol.saveXYZ( fname, comment=f"ang {a}", mode='a' )
 
-def scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2], bEpairs=False ):
+def scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2], bEpairs=False, bFireball=False ):
     fname = path+"angdistscan_"+name
     #r1 = mol.apos[j1] - mol.apos[i]
     #r2 = mol.apos[j2] - mol.apos[i]
@@ -73,25 +75,41 @@ def scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2], bEpairs=False ):
         r = np.linalg.norm(v)
         vs.append(v/r)
         #rs.append(r)
+    if bFireball:
+        fc.initialize( atomType=mol.atypes, atomPos=mol.apos, verbosity=3 )
+        fc.evalForce( mol.apos, nmax_scf=100 )
+        Emap = np.zeros( (len(angs),len(dists)) )
+
     mol.saveXYZ( fname, mode='w' )
     if bEpairs:
         mol.findBonds()
-    for a in angs:
+    for iang,a in enumerate(angs):
         ca = np.cos(a)
         sa = np.sin(a)
-        for r in dists:
+        for ir,r in enumerate(dists):
+            label = f"# ang {a:.3f} dist {r:.3f}"
             for jj,j in enumerate(jbs):
                 mol.apos[j,:] = mol.apos[ia,:] + ax*(r*ca) + vs[jj]*(r*sa)
+            if bFireball:
+                #print("============= CALL FireBall ", r, a)
+                forces, Es = fc.evalForce( mol.apos, nmax_scf=100 )
+                #print( "Forces:", forces )
+                #print( "Energies:", Es, Es[1:].sum() )
+                #print( f"FireBall( a: {a} r: {r} ) Energies:", Es )
+                Emap[iang,ir] = Es[0]
+                label += f" Etot  {Es[0]:.3f}"
+                print( "FireBall " + label )
             if bEpairs:
                 mol_ = mol.clonePBC()
                 mol_.bonds = mol.bonds
                 mol_.neighs()
                 mol_.add_electron_pairs()
-                mol_.saveXYZ( fname, comment=f"ang {a} dist {r}", mode='a' )
+                mol_.saveXYZ( fname, comment=label, mode='a' )
             else:    
-                mol.saveXYZ( fname, comment=f"ang {a} dist {r}", mode='a' )
-
-
+                mol.saveXYZ( fname, comment=label, mode='a' )
+    
+    if bFireball:
+        return Emap
 
 # find all molecules in path
 #molecules = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
@@ -112,8 +130,20 @@ for name in molecules:
     # scan_group_distance( mol, groups, l0=-0.4, dl=0.1 )
 
     # scan angle
-    angs = np.linspace(np.pi/4, np.pi*7./8., 10)
-    dists = np.linspace(0.5,1.5,10)
+    angs = np.linspace(np.pi/4, np.pi*7./8., 20)
+    dists = np.linspace(0.7,2.5,20)
     #scan_angle( mol, angs, ia=0, j=1, jbs=[2] )
     #scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2] )
-    scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2], bEpairs=True )
+    #scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2], bEpairs=True )
+    Emap = scan_angle_dist( mol, angs, dists, ia=0, j=1, jbs=[2], bFireball=True )
+
+    import matplotlib.pyplot as plt
+    rad2deg = 180./np.pi
+    Emin = Emap.min()
+    Emax = Emin+5.0
+    plt.imshow( Emap, origin='lower', extent=[dists[0]*100,dists[-1]*100,angs[0]*rad2deg,angs[-1]*rad2deg], vmin=Emin, vmax=Emax )
+    plt.colorbar()
+    plt.xlabel( "Distance [pm]" )
+    plt.ylabel( "Angle [deg.]" )
+    plt.title( name )
+    plt.show()
