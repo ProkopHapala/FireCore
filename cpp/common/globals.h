25c61ae6657866546380eed348c2260d14515eb0
ProkopHapala <ProkopHapala@gmail.com>
2025-06-24 11:58:03 +0200

    testing new _assert() macro which can execute any action, and is compiled only in DEBUG mode

--- cpp/common/globals.h
+++ cpp/common/globals.h
@@ -11,5 +11,17 @@ static char tmpstr[ntmpstr];
 
 static double tick2second=1e-9;
 
+// depending on debug / optimization leval 
+#ifdef DEBUGBUILD
+#define _assert( cond, action ) \
+    if( !(cond) ){ \
+        printf("Assertion failed: %s, line: %d  function: %s file: %s \n", #cond, __LINE__, __FUNCTION__, __FILE__ ); \
+        printf("  => execute action: %s", #action ); \
+        {action;} \
+    }
+#else
+#define _assert( cond, action )
+#endif
+
 
 //#endif
